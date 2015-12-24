f108fd3174c4c27fb704876197a6169b31633ad0
Alexander Kazachkin <alexander.kazachkin@citeck.ru>
2015-12-24 17:46:20 +0600

    issue #6825 - support incoming json as for criteria-search

--- idocs-repo/config/alfresco/templates/webscripts/ru/citeck/light-search/light-search.post.json.js
+++ idocs-repo/config/alfresco/templates/webscripts/ru/citeck/light-search/light-search.post.json.js
@@ -10,6 +10,14 @@
     return string.replace(/[:]/g, "\\$&");
   }
 
+  function buildAttribute(string) {
+    if (string.indexOf(":") != -1) {
+      return "@" + escape(string);
+    } else {
+      return string.toUpperCase();
+    }
+  }
+
   function applyPredicate(value, predicate) {
     switch (predicate) {
       case "string-contains":
@@ -27,42 +35,59 @@
     }
   }
 
-  if (!json.has("criteria")) {
-    exit(status.STATUS_BAD_REQUEST, "Argument 'criteria' should be specified")
-    return;
-  }
-
-  if (!json.has("type")) {
-    exit(status.STATUS_BAD_REQUEST, "Argument 'type' should be specified")
-    return;
-  }
-
   var object = eval("(" + json.toString() + ")"),
-      criteria = object.criteria,
-      maxItems = object.maxItems || 10,
-      skipCount = object.skipCount || 0,
-      searchQuery = "TYPE: \"" + object.type + "\" AND";
+      criteria = object.criteria || [],
+      maxItems = 10, skipCount = 0,
+      searchQuery = "";
 
-  searchQuery += " (";
-  for (var c = 0; c < criteria.length; c++) {
+  // criteria-search mode
+  if (criteria.length == 0) {
+    // build list of criterion
+    for (var key in object) {
+      if (object[key] && object[key] != "undefined" && object[key] != "null") {
+        if (key == "maxItems") maxItems = object[key];
+        if (key == "skipCount") skipCount = object[key];
+
+        if (/(field|predicate|value)_\d+/.test(key)) {
+          var index = key.split("_")[1],
+              name  = key.split("_")[0];
+         
+          if (!criteria[index]) { criteria[index] = {} };
+          if (name == "field") { name = "attribute" };
+          criteria[index][name] = object[key];       
+        }
+      }
+    }
+
+    // search TYPE criterion
+    for (var i in criteria) {
+      if (criteria[i].attribute == "TYPE" && criteria[i].predicate == "type-equals") {
+        searchQuery += "TYPE: \"" + criteria[i].value + "\" AND";
+
+        // remove TYPE criterion from list
+        criteria.splice(i, 1);
+      }
+    }
+  } else {
+    if (object.type) searchQuery += "TYPE: \"" + object.type + "\" AND";
+  }
+
+  // light-search mode
+  if (searchQuery.indexOf("AND") != -1) searchQuery += " (";
+  for (var c in criteria) {
     var criterion = criteria[c];
 
     if (criterion.attribute && criterion.predicate && criterion.value) {
-      var attribute = (criterion.attribute.indexOf(":") != -1 ? "@" : "") + escape(criterion.attribute);
-      searchQuery += attribute + ": \"" + applyPredicate(criterion.value, criterion.predicate) + "\"";
-
-      if (c != criteria.length - 1) searchQuery += " OR ";
+      if (c != 0) searchQuery += " OR ";
+      searchQuery += buildAttribute(criterion.attribute) + ": \"" + applyPredicate(criterion.value, criterion.predicate) + "\"";
     }  
   }
-  searchQuery += ")";
+  if (searchQuery.indexOf("AND") != -1) searchQuery += ")";
 
   var result = search.query({
     query: searchQuery,
     language: "lucene",
-    page: {
-      maxItems: maxItems,
-      skipCount: skipCount
-    }
+    page: { maxItems: maxItems, skipCount: skipCount }
   })
 
   model.query = searchQuery;
