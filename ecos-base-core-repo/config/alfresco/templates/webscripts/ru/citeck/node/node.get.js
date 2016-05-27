f12d0aad253d3fe6ab54c78ddbf83b7b34701188
Alexander Kazachkin <alexander.kazachkin@citeck.ru>
2016-05-27 19:36:35 +0600

    issue #7722 - Customize component 'bch-details'  for new model - web-script 'citeck/node' support parameter 'children'

--- ecos-base-core-repo/config/alfresco/templates/webscripts/ru/citeck/node/node.get.js
+++ ecos-base-core-repo/config/alfresco/templates/webscripts/ru/citeck/node/node.get.js
@@ -1,91 +1,110 @@
 (function() {
-	var extractNames = function(values) {
-		var result = [];
-		if (values) {
-			values = ('' + values).replace(/^\s+|\s+$/g, '');
-			var valuesArr = values.split(',');
-			for (var i = 0; i < valuesArr.length; i++) {
-				var valueName = valuesArr[i].replace(/^\s+|\s+$/g, '');
-				if (valueName)
-					result.push(valueName);
-			}
+
+
+if (args['nodeRef']) {
+	var node = search.findNode(args['nodeRef']);
+
+	if (node) {
+		model.node = node;
+		model.siteShortName = node.siteShortName;
+
+		var propsArr 			 = args['props'] ? extractNames(args['props']) : extractKeys(node.properties), 
+				assocsArr 		 = args['assocs'] ? extractNames(args['assocs']) : extractKeys(node.assocs), 
+				childAssocsArr = args['childAssocs'] ? extractNames(args['childAssocs']) : extractKeys(node.childAssocs);
+
+		var propsResult 			= [],
+				assocsResult 			= [],
+				childAssocsResult = [];
+		
+		for (var i = 0; i < propsArr.length; i++) {
+			var prop = propsArr[i];
+			propsResult.push({
+				key: utils.shortQName(prop),
+				value: node.properties[prop]
+			});
 		}
-		return result;
-	}
 
-	var nodeRef = args['nodeRef'],
-		props = args['props'],
-		assocs = args['assocs'],
-		childAssocs = args['childAssocs'],
-		propsResult = [],
-		assocsResult = [],
-		childAssocsResult = [],
-		siteShortName = null;
-
-	if (nodeRef) {
-		var node = search.findNode(nodeRef);
-		if (node) {
-			model.node = node;
-			var propsArr = [],
-				assocsArr = [],
-				childAssocsArr = [];
-			siteShortName = node.siteShortName;
-			if (props || assocs || childAssocs) {
-				// Returns only specified properties and child associations
-				propsArr = extractNames(props);
-				assocsArr = extractNames(assocs);
-				childAssocsArr = extractNames(childAssocs);
-			}
-			else {
-				// We are going to return all properties and child associations
-				for (var prop in node.properties)
-					propsArr.push(prop);
-				for (var assoc in node.assocs)
-					assocsArr.push(assoc);
-				for (var childAssoc in node.childAssocs)
-					childAssocsArr.push(childAssoc);
-			}
-			for (var i = 0; i < propsArr.length; i++) {
-				var prop = propsArr[i];
-				propsResult.push({
-					key: utils.shortQName(prop),
-					value: node.properties[prop]
+		for (var i = 0; i < assocsArr.length; i++) {
+			var assoc = assocsArr[i];
+			var assocObj = node.assocs[assoc];
+			if (assocObj) {
+				var nodeRefs = [];
+				if (assocObj.length > 0) {
+					for (var j = 0; j < assocObj.length; j++)
+						nodeRefs.push(assocObj[j].nodeRef.toString());
+				}
+				assocsResult.push({
+					key: utils.shortQName(assoc),
+					value: nodeRefs
 				});
 			}
-			for (var i = 0; i < assocsArr.length; i++) {
-				var assoc = assocsArr[i];
-				var assocObj = node.assocs[assoc];
-				if (assocObj) {
-					var nodeRefs = [];
-					if (assocObj.length > 0) {
-						for (var j = 0; j < assocObj.length; j++)
-							nodeRefs.push(assocObj[j].nodeRef.toString());
-					}
-					assocsResult.push({
-						key: utils.shortQName(assoc),
-						value: nodeRefs
-					});
+		}
+
+		for (var i = 0; i < childAssocsArr.length; i++) {
+			var assoc = childAssocsArr[i];
+			var assocObj = node.childAssocs[assoc];
+			if (assocObj) {
+				var nodeRefs = [];
+				if (assocObj.length > 0) {
+					for (var j = 0; j < assocObj.length; j++)
+						nodeRefs.push(assocObj[j].nodeRef.toString());
 				}
-			}
-			for (var i = 0; i < childAssocsArr.length; i++) {
-				var assoc = childAssocsArr[i];
-				var assocObj = node.childAssocs[assoc];
-				if (assocObj) {
-					var nodeRefs = [];
-					if (assocObj.length > 0) {
-						for (var j = 0; j < assocObj.length; j++)
-							nodeRefs.push(assocObj[j].nodeRef.toString());
-					}
-					childAssocsResult.push({
-						key: utils.shortQName(assoc),
-						value: nodeRefs
-					});
-				}
+				childAssocsResult.push({
+					key: utils.shortQName(assoc),
+					value: nodeRefs
+				});
 			}
 		}
+
+		if (args["children"]) {
+			var onlyChildren = extractNames(args["children"]),
+					onlyChildrenResult = [];
+
+			for (var i = 0; i < onlyChildren.length; i++) {
+				for (var c = 0; c < node.children.length; c++) {
+					if (node.children[c].typeShort == onlyChildren[i]); 
+						onlyChildrenResult.push(node.children[c]);
+						break;
+				}
+			}
+
+			model.children = onlyChildrenResult;
+		} else { model.children = node.children }
+
 	}
-	model.props = propsResult;
-	model.assocs = assocsResult;
-	model.childAssocs = childAssocsResult;
-	model.siteShortName = siteShortName;
+}
+
+model.props 			= propsResult;
+model.assocs 			= assocsResult;
+model.childAssocs = childAssocsResult;
+
 })();
+
+
+// -----------------
+// Private Functions
+// ----------------- 
+
+function extractNames(values) {
+	var result = [];
+	if (values) {
+		values = ('' + values).replace(/^\s+|\s+$/g, '');
+		var valuesArr = values.split(',');
+		for (var i = 0; i < valuesArr.length; i++) {
+			var valueName = valuesArr[i].replace(/^\s+|\s+$/g, '');
+			if (valueName)
+				result.push(valueName);
+		}
+	}
+	return result;
+};
+
+function extractKeys(values) {
+	var result = [];
+
+	if (values instanceof Array) {
+		for (var v = 0; v < values.length; v++) result.push(values[v++].typeShort);
+	} else { for (var v in values) result.push(v) }
+	
+	return result;
+};
