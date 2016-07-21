3cabd23bb76b73f10ead722fefd18566daeafa6e
Alexander Kazachkin <alexander.kazachkin@citeck.ru>
2016-07-21 14:26:45 +0700

    issue ECOSCOM-488 - Refactor header menu to aikau - fix journals-extension-module

--- journals-share/config/alfresco/site-webscripts/ru/citeck/components/header-journals/share-header.get.js
+++ journals-share/config/alfresco/site-webscripts/ru/citeck/components/header-journals/share-header.get.js
@@ -1,13 +1,16 @@
 if (user.isAdmin) {
-     var headerMenu = widgetUtils.findObject(model.jsonModel, "id", "HEADER_APP_MENU_BAR");
-    if (headerMenu != null) {
-        headerMenu.config.widgets.push({
-            id: "HEADER_CUSTOM_PROFILE_LINK",
-            name: "alfresco/menus/AlfMenuBarItem",
-            config: {
-                label: msg.get("header.meta_journals.label"),
-                targetUrl: "journals2/list/meta"
-            }
-        });
-    }
+  var headerMenu = widgetUtils.findObject(model.jsonModel, "id", "HEADER_APP_MENU_BAR"),
+      metajournals = widgetUtils.findObject(model.jsonModel, "id", "HEADER_MORE_TOOLS_META_JOURNALS");
+
+  if (headerMenu && !metajournals) {
+    headerMenu.config.widgets.push({
+      id: "HEADER_MORE_TOOLS_META_JOURNALS",
+      name: "alfresco/menus/AlfMenuBarItem",
+      config: {
+        id: "HEADER_MORE_TOOLS_META_JOURNALS",
+        label: "header.meta_journals.label",
+        targetUrl: "journals2/list/meta"
+      }
+    });
+  }
 }
