d95bbf6fab45d9f12bb02cc4749ae6600b8c15ff
Alexander Kazachkin <alexander.kazachkin@citeck.ru>
2016-07-25 14:38:29 +0700

    issue ECOSCOM-488 - Refactor header menu to aikau - library for adding items in menu and admin group - additional function for journal extension module

--- journals-share/config/alfresco/site-webscripts/ru/citeck/components/header-journals/share-header.get.js
+++ journals-share/config/alfresco/site-webscripts/ru/citeck/components/header-journals/share-header.get.js
@@ -3,8 +3,14 @@ if (user.isAdmin) {
       metajournals = widgetUtils.findObject(model.jsonModel, "id", "HEADER_MORE_TOOLS_META_JOURNALS");
 
   if (headerMenu && !metajournals) {
-    headerMenu.config.widgets.push({
-      id: "HEADER_MORE_TOOLS_META_JOURNALS",
+    var indexOfLastElement = headerMenu.config.widgets.length - 1,
+        lastElement = function() { return headerMenu.config.widgets[indexOfLastElement] };
+
+    if (lastElement().config.label == "Debug Menu") indexOfLastElement--;
+    if (lastElement().id && lastElement.id == "HEADER_MORE") indexOfLastElement--;
+
+    headerMenu.config.widgets.splice(indexOfLastElement, 0, {
+      id: "HEADER_META_JOURNALS",
       name: "alfresco/menus/AlfMenuBarItem",
       config: {
         label: "header.meta_journals.label",
