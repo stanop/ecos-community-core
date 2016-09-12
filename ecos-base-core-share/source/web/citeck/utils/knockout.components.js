1a28e0956cf015a98258baefd5bf574ec6ccdb8d
Alexander Kazachkin <alexander.kazachkin@citeck.ru>
2016-09-12 14:44:21 +0700

    issue ECOSCOM-577 - Transfer filters in Journal Control to the Citeck EcoS components

--- ecos-base-core-share/source/web/citeck/utils/knockout.components.js
+++ ecos-base-core-share/source/web/citeck/utils/knockout.components.js
@@ -72,7 +72,9 @@ define(['lib/knockout'], function(ko) {
                         dataObj: query,
                         successCallback: {
                             scope: this,
-                            fn: function(response) { this.nestedViewModel.options(response.json.results); }
+                            fn: function(response) { 
+                                this.nestedViewModel.options(response.json ? response.json.results : []);
+                            }
                         }
                     });
                 }
@@ -99,6 +101,10 @@ define(['lib/knockout'], function(ko) {
         template: 
            '<div class="criterion-value" data-bind="attr: { id: valueContainerId }"></div>'
     });
+
+
+    // TODO:
+    // - refactoring 'filter-criteria' and 'list-of-selected-criterion'. Combine methods
 
     ko.components.register("filter-criteria", {
         viewModel: function(params) {
@@ -151,6 +157,206 @@ define(['lib/knockout'], function(ko) {
                     }} --><!-- /ko -->\
                 </div>\
             </div>'
+    });
+
+    ko.components.register('list-of-selected-criterion', {
+        viewModel: function(params) {
+            var self = this;
+            initializeParameters.call(this, params);
+
+            self.remove = function(data, event) {
+                self.selectedFilterCriteria.remove(data);
+            };
+
+            this.nodetype = function(data) {
+                return ko.computed(function() {
+                    return self.journalType.attribute(data.name()).nodetype();
+                });
+            }
+        },
+        template: 
+           '<table class="selected-criteria-list">\
+                <tbody>\
+                    <!-- ko foreach: selectedFilterCriteria -->\
+                        <tr>\
+                            <td class="action-col"><a class="remove-selected-criterion" data-bind="click: $component.remove">X</a></td>\
+                            <td class="name-col"><span class="selected-criterion-name" data-bind="text: displayName"></span></td>\
+                            <td class="predicate-col" data-bind="with: datatype">\
+                                <select class="predicate" data-bind="options: predicates,\
+                                                                     optionsText: \'label\',\
+                                                                     optionsValue: \'id\',\
+                                                                     value: $parent.predicateValue"></select>\
+                            </td>\
+                            <td class="value-col">\
+                                <!-- ko component: { name: "filter-criterion-value", params: {\
+                                    fieldId: $component.htmlId + "-criterion-" + $index(),\
+                                    datatype: resolve(\'datatype.name\', null),\
+                                    nodetype: $component.nodetype($data),\
+                                    value: value\
+                                }} --><!-- /ko -->\
+                            </td>\
+                        </tr>\
+                    <!-- /ko -->\
+                </tbody>\
+            </table>'
+    });
+
+    ko.components.register('journal', {
+        viewModel: function(params) {
+            if (!params.sourceElements && !params.journalType) {
+                throw new Error("Required parameters are missing");
+                return;
+            }
+
+            var self = this;
+            initializeParameters.call(this, params);
+
+            // options
+            self.options = {
+                multiple: false,
+                pagination: false,
+                loading: false,
+                localization: { 
+                    nextPageLabel: "-->", 
+                    nextPageTitle: "-->", 
+                    previousPageLabel: "<--", 
+                    previousPageTitle: "<--" 
+                } 
+            };
+            Citeck.utils.concatOptions(self.options, params.options);
+
+            if (!self.loading) { self.loading = self.options.loading; }
+
+            // methods
+            self.selectElement = function(data, event) {
+                if (self.targetElements) {
+                    if (self.options.multiple && (ko.isObservable(self.options.multiple) ? self.options.multiple() : self.options.multiple)) {
+                        if (self.targetElements.indexOf(data) == -1) self.targetElements.push(data);
+                    } else {
+                        self.targetElements([data]);
+                    }
+                };
+
+                if (self.callback) self.callback(data, event);
+            };
+
+            self.nextPage = function(data, event) {
+                self.page(self.page() + 1);
+            };
+
+            self.previousPage = function(data, event) {
+                self.page(self.page() - 1);
+            };
+
+            self.displayText = function(value, attr) {
+                if (value) {
+                    // if string
+                    if (typeof value == "string") {
+                        if (attr.labels() && attr.labels()[value]) 
+                            return attr.labels()[value];
+                    }
+
+                    // if object
+                    if (typeof value == "object") {
+                        if (value instanceof Date) return value.toLocaleString();
+                        if (isInvariantsObject(value)) return value.name;
+                    }
+
+                    return value;
+                }
+
+                return null;
+            };
+        },
+        template:
+           '<!-- ko if: loading -->\
+                <div class="loading"></div>\
+            <!-- /ko -->\
+            <table class="journal">\
+                <thead>\
+                    <!-- ko if: columns ? true : false -->\
+                        <tr data-bind="foreach: columns">\
+                            <!-- ko if: $component.journalType.attribute($data) -->\
+                                <!-- ko with: $component.journalType.attribute($data) -->\
+                                    <th data-bind="text: displayName"></th>\
+                                <!-- /ko -->\
+                            <!-- /ko -->\
+                        </tr>\
+                    <!-- /ko -->\
+                    <!-- ko ifnot: columns ? true : false -->\
+                        <tr data-bind="foreach: $component.journalType.defaultAttributes">\
+                            <th data-bind="text: displayName"></th>\
+                        </tr>\
+                    <!-- /ko -->\
+                </thead>\
+                <tbody data-bind="foreach: sourceElements">\
+                    <!-- ko if: $component.columns ? true : false -->\
+                        <tr class="journal-element" data-bind="attr: { id: nodeRef },\
+                                                               foreach: $component.columns,\
+                                                               click: $component.selectElement, clickBubble: false">\
+                           <!-- ko if: $component.journalType.attribute($data) ? true : false -->\
+                                <!-- ko with: $component.journalType.attribute($data) -->\
+                                    <!-- ko if: $parents[1].properties[$data.name()] -->\
+                                        <td data-bind="text: $component.displayText($parents[1].properties[$data.name()], $data)"></td>\
+                                    <!-- /ko -->\
+                                    <!-- ko ifnot: $parents[1].properties[$data.name()] -->\
+                                        <!-- ko with: $parents[1].impl().attribute($data.name()) -->\
+                                            <td data-bind="text: $data.valueTitle() || $data.textValue()"></td>\
+                                        <!-- /ko -->\
+                                    <!-- /ko -->\
+                                <!-- /ko -->\
+                            <!-- /ko -->\
+                        </tr>\
+                    <!-- /ko -->\
+                    <!-- ko ifnot: $component.columns ? true : false -->\
+                        <tr class="journal-element" data-bind="attr: { id: nodeRef },\
+                                                               foreach: $component.journalType.defaultAttributes,\
+                                                               click: $component.selectElement, clickBubble: false">\
+                            <!-- ko if: $parent.properties[$data.name()] -->\
+                                <td data-bind="text: $component.displayText($parent.properties[$data.name()], $data)"></td>\
+                            <!-- /ko -->\
+                            <!-- ko ifnot: $parent.properties[$data.name()] -->\
+                                <!-- ko with: $parent.impl().attribute($data.name()) -->\
+                                    <td data-bind="text: $data.valueTitle() || $data.textValue()"></td>\
+                                <!-- /ko -->\
+                            <!-- /ko -->\
+                        </tr>\
+                    <!-- /ko -->\
+                </tbody>\
+            </table>\
+            <!-- ko if: options.pagination && sourceElements -->\
+                <!-- ko with: sourceElements().pagination -->\
+                    <!-- ko if: ($component.page() - 1 > 0) || hasMore -->\
+                        <div class="journal-pagination">\
+                            <span class="previous-page">\
+                                <!-- ko if: $component.page() - 1 > 0 -->\
+                                    <a data-bind="click: $component.previousPage,\
+                                                  text: $component.options.localization.previousPageLabel,\
+                                                  attr: { title: $component.options.localization.previousPageTitle }"><--</a>\
+                                <!-- /ko -->\
+                                <!-- ko ifnot: $component.page() - 1 > 0 -->\
+                                    <!-- ko text: $component.options.localization.previousPageLabel --><!-- /ko -->\
+                                <!-- /ko -->\
+                            </span>\
+                            <span class="page-label">\
+                                <span class="start-page" data-bind="text: $component.page() * maxItems - maxItems + 1"></span>\
+                                <span class="dash">-</span>\
+                                <span class="end-page" data-bind="text: $component.page() * maxItems"></span>\
+                            </span>\
+                            <span class="next-page">\
+                                <!-- ko if: hasMore -->\
+                                    <a data-bind="click: $component.nextPage,\
+                                                  text: $component.options.localization.nextPageLabel,\
+                                                  attr: { title: $component.options.localization.nextPageTitle }">--></a>\
+                                <!-- /ko -->\
+                                <!-- ko ifnot: hasMore -->\
+                                    <!-- ko text: $component.options.localization.nextPageLabel --><!-- /ko -->\
+                                <!-- /ko -->\
+                            </span>\
+                        </div>\
+                    <!-- /ko -->\
+                <!-- /ko -->\
+            <!-- /ko -->'
     });
 
 
@@ -319,6 +525,13 @@ define(['lib/knockout'], function(ko) {
                                       "optionsText: optionsText, optionsValue: optionsValue");
         return fixedHTML;
     }
+
+    function isInvariantsObject(object) {
+        return object.toString().toLowerCase().indexOf("invariants") != -1
+    }
+
+
+
 });
 
 
