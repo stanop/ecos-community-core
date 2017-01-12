c6c7d149a49ff2a6a22901bb783c1d2d59718fa8
Maxim Strizhov <maxim.strizhov@citeck.ru>
2017-01-12 15:20:32 +0700

    Bugfix: added OnCreateNode handler

--- idocs-repo/source/java/ru/citeck/ecos/behavior/role/UpdateRolesBehaviour.java
+++ idocs-repo/source/java/ru/citeck/ecos/behavior/role/UpdateRolesBehaviour.java
@@ -3,10 +3,12 @@ package ru.citeck.ecos.behavior.role;
 import org.alfresco.repo.node.NodeServicePolicies.OnUpdatePropertiesPolicy;
 import org.alfresco.repo.node.NodeServicePolicies.OnCreateAssociationPolicy;
 import org.alfresco.repo.node.NodeServicePolicies.OnDeleteAssociationPolicy;
+import org.alfresco.repo.node.NodeServicePolicies.OnCreateNodePolicy;
 import org.alfresco.repo.policy.Behaviour.NotificationFrequency;
 import org.alfresco.repo.policy.OrderedBehaviour;
 import org.alfresco.repo.policy.PolicyComponent;
 import org.alfresco.service.cmr.repository.AssociationRef;
+import org.alfresco.service.cmr.repository.ChildAssociationRef;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.namespace.QName;
 import org.apache.commons.logging.Log;
@@ -22,7 +24,8 @@ import java.util.Map;
  */
 public class UpdateRolesBehaviour implements OnUpdatePropertiesPolicy,
                                              OnCreateAssociationPolicy,
-                                             OnDeleteAssociationPolicy {
+                                             OnDeleteAssociationPolicy,
+                                             OnCreateNodePolicy {
 
     private static final Log LOGGER = LogFactory.getLog(UpdateRolesBehaviour.class);
     private static final int ORDER = 100;
@@ -48,6 +51,11 @@ public class UpdateRolesBehaviour implements OnUpdatePropertiesPolicy,
                 ICaseRoleModel.ASPECT_HAS_ROLES,
                 new OrderedBehaviour(this, "onDeleteAssociation", NotificationFrequency.TRANSACTION_COMMIT, ORDER)
         );
+        this.policyComponent.bindAssociationBehaviour(
+                OnCreateNodePolicy.QNAME,
+                ICaseRoleModel.ASPECT_HAS_ROLES,
+                new OrderedBehaviour(this, "onCreateNode", NotificationFrequency.TRANSACTION_COMMIT, ORDER)
+        );
     }
 
     @Override
@@ -63,6 +71,11 @@ public class UpdateRolesBehaviour implements OnUpdatePropertiesPolicy,
     @Override
     public void onUpdateProperties(NodeRef nodeRef, Map<QName, Serializable> map, Map<QName, Serializable> map1) {
         caseRoleService.updateRoles(nodeRef);
+    }
+
+    @Override
+    public void onCreateNode(ChildAssociationRef childAssociationRef) {
+        caseRoleService.updateRoles(childAssociationRef.getChildRef());
     }
 
     public void setPolicyComponent(PolicyComponent policyComponent) {
