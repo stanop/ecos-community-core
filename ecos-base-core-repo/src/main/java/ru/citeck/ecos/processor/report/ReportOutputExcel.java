d268528cfd998153179843a8fb92b8bfe28ec532
Pavel Marchuk <pavel.marchuk@citeck.ru>
2017-01-23 12:31:30 +0700

    fix classcastException

--- ecos-base-core-repo/src/main/java/ru/citeck/ecos/processor/report/ReportOutputExcel.java
+++ ecos-base-core-repo/src/main/java/ru/citeck/ecos/processor/report/ReportOutputExcel.java
@@ -173,7 +173,7 @@ public class ReportOutputExcel extends AbstractDataBundleLine {
 	    				
 	    				String dataType = (String) cellData.get(ReportProducer.DATA_TYPE_ATTR);
 						CellStyle cellStyle = newCell.getCellStyle();
-						String valStr =  (String) cellData.get(ReportProducer.DATA_VALUE_ATTR);
+						String valStr = String.valueOf(cellData.get(ReportProducer.DATA_VALUE_ATTR));
 						if (valStr != null && !valStr.isEmpty()) {
 							if ("Integer".equals(dataType)) {
 								try {
