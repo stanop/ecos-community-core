2bc48fe8bae0816d791d7146f031f5a8e0e096ec
Valentin Skeeba <valentin.skeeba@citeck.ru>
2017-03-06 10:02:49 +0300

    Ecos config Service rework

--- idocs-repo/source/java/ru/citeck/ecos/config/EcosConfigSearchService.java
+++ idocs-repo/source/java/ru/citeck/ecos/config/EcosConfigSearchService.java
@@ -2,31 +2,19 @@ package ru.citeck.ecos.config;
 
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
-import org.alfresco.service.cmr.search.SearchService;
-import org.alfresco.service.namespace.QName;
+import org.alfresco.service.cmr.repository.StoreRef;
+import org.alfresco.service.cmr.search.*;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import ru.citeck.ecos.search.*;
+import ru.citeck.ecos.model.ConfigModel;
 
 public class EcosConfigSearchService {
 
     private static final Log LOGGER = LogFactory.getLog(EcosConfigSearchService.class);
 
-    private static final String CONFIG_NAMESPASE = "http://www.citeck.ru/model/config/1.0";
-
-    private SearchCriteriaFactory searchCriteriaFactory;
-
-    private CriteriaSearchService criteriaSearchService;
+    private SearchService searchService;
 
     private NodeService nodeService;
-
-    public void setSearchCriteriaFactory(SearchCriteriaFactory searchCriteriaFactory) {
-        this.searchCriteriaFactory = searchCriteriaFactory;
-    }
-
-    public void setCriteriaSearchService(CriteriaSearchService criteriaSearchService) {
-        this.criteriaSearchService = criteriaSearchService;
-    }
 
     public void setNodeService(NodeService nodeService) {
         this.nodeService = nodeService;
@@ -35,13 +23,21 @@ public class EcosConfigSearchService {
     public Object getParamValue(String key) {
         String result = null;
         try {
-            SearchCriteria searchCriteria = searchCriteriaFactory.createSearchCriteria();
-            searchCriteria.addCriteriaTriplet(FieldType.TYPE, SearchPredicate.TYPE_EQUALS, QName.createQName(CONFIG_NAMESPASE, "ecosConfig"))
-                    .addCriteriaTriplet(QName.createQName(CONFIG_NAMESPASE, "key"), SearchPredicate.STRING_EQUALS, key);
-            CriteriaSearchResults searchResults = criteriaSearchService.query(searchCriteria, SearchService.LANGUAGE_LUCENE);
-            if (searchResults.getResults() != null && !searchResults.getResults().isEmpty()) {
-                for (NodeRef settingNodeRef : searchResults.getResults()) {
-                    result = ((String) nodeService.getProperty(settingNodeRef, QName.createQName(CONFIG_NAMESPASE, "value")));
+
+            SearchParameters searchParameters = new SearchParameters();
+            searchParameters.setLanguage(SearchService.LANGUAGE_FTS_ALFRESCO);
+            searchParameters.addStore(StoreRef.STORE_REF_WORKSPACE_SPACESSTORE);
+            searchParameters.setQueryConsistency(QueryConsistency.TRANSACTIONAL);
+            searchParameters.setLimitBy(LimitBy.UNLIMITED);
+            searchParameters.setLimit(0);
+            searchParameters.setMaxPermissionChecks(Integer.MAX_VALUE);
+            searchParameters.setMaxPermissionCheckTimeMillis(Integer.MAX_VALUE);
+            searchParameters.setMaxItems(-1);
+            searchParameters.setQuery("TYPE:\"" + ConfigModel.TYPE_ECOS_CONFIG + "\" AND =@" + ConfigModel.PROP_KEY + ":" + key);
+            ResultSet searchResults = searchService.query(searchParameters);
+            if (searchResults.getNodeRefs() != null && !searchResults.getNodeRefs().isEmpty()) {
+                for (NodeRef settingNodeRef : searchResults.getNodeRefs()) {
+                    result = ((String) nodeService.getProperty(settingNodeRef, ConfigModel.PROP_VALUE));
                 }
             }
         } catch (Exception e) {
@@ -49,5 +45,9 @@ public class EcosConfigSearchService {
             throw e;
         }
         return result;
+    }
+
+    public void setSearchService(SearchService searchService) {
+        this.searchService = searchService;
     }
 }
