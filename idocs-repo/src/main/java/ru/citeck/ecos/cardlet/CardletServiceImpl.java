202ea5e1c83ae9fffc5b57a275a4c9b7802a46d9
Alexander Nemerov <alexander.nemerov@citeck.ru>
2017-03-13 16:44:58 +0700

    Bugfixes:  1. 6.10 -> ${dependency.surf.version} to  2. custom-actions  3. cardlets search

--- idocs-repo/src/main/java/ru/citeck/ecos/cardlet/CardletServiceImpl.java
+++ idocs-repo/src/main/java/ru/citeck/ecos/cardlet/CardletServiceImpl.java
@@ -162,6 +162,7 @@ import java.util.*;
     private List<NodeRef> queryCardModes(List<QName> types, Collection<String> authorities) {
         String typeClause = "TYPE:\"" + CardletModel.TYPE_CARD_MODE + "\"";
         String documentClause = disjunction(CardletModel.PROP_ALLOWED_TYPE, types, false, true);
+        authorities.add("");
         String authorityClause = disjunction(CardletModel.PROP_ALLOWED_AUTHORITIES, authorities, false, true);
         String query = typeClause + " AND " + documentClause + " AND " + authorityClause;
         if (logger.isDebugEnabled()) {
