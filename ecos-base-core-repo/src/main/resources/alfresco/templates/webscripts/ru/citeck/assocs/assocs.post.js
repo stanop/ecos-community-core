70c8639913769d2d8b8555fff4ee6e8345f3d1af
Maxim Strizhov <maxim.strizhov@citeck.ru>
2017-06-19 18:27:42 +0700

    ECOSCOM-877 - Create association webscript didn't work correctly

--- ecos-base-core-repo/src/main/resources/alfresco/templates/webscripts/ru/citeck/assocs/assocs.post.js
+++ ecos-base-core-repo/src/main/resources/alfresco/templates/webscripts/ru/citeck/assocs/assocs.post.js
@@ -4,39 +4,51 @@
         var sourceRefs = args['sourceRef'];
         var targetRefs = args['targetRef'];
         var assocType = args['assocTypes'];
-        if (assocType == undefined) assocType = "";
+        if (assocType === undefined) assocType = "";
         var source = '';
         var target = '';
         var allCreate = 'true';
-        if(sourceRefs != null && targetRefs != null) {
-            for each(source in sourceRefs.split(','))
-                if (source != '' && source.length != 0 ){
+        if(sourceRefs !== null && targetRefs !== null) {
+            var sourceRefsArr = sourceRefs.split(',');
+            for (var i = 0; i < sourceRefsArr.length; i++) {
+                source = sourceRefsArr[i];
+                if (source !== null && source !== '' && source.length !== 0) {
                     var sourceNode = search.findNode(source);
                     if (sourceNode !== null) {
                         var existTargets = [];
                         existTargets = sourceNode.assocs[assocType];
-                        for each(target in targetRefs.split(','))
-                            if (target != '' && target.length != 0 && source != target){
+                        var targerRefsArr = targetRefs.split(',');
+                        for (var j = 0; j < targerRefsArr.length; j++) {
+                            target = targerRefsArr[j];
+                            if (target !== null && target !== '' && target.length !== 0 && source !== target) {
                                 var targetNode = search.findNode(target);
                                 if (targetNode !== null) {
                                     var exist = false;
-                                    for each (tar in existTargets)
-                                        if(tar.nodeRef == target) {
-                                            exist = true;
+                                    if (existTargets !== null) {
+                                        var tar;
+                                        for (var k = 0; k < existTargets.length; k++) {
+                                            tar = existTargets[k];
+                                            if (tar.nodeRef === target) {
+                                                exist = true;
+                                            }
                                         }
-                                    if(!exist) {
+                                    }
+                                    if (!exist) {
                                         sourceNode.createAssociation(targetNode, assocType);
                                     } else {
                                         allCreate = "false";
                                     }
                                 }
                             }
+                        }
                     }
                 }
-            model.data = allCreate;
+            }
         }
-    } catch (ex){
+        model.data = allCreate;
+    } catch (ex) {
+        logger.error(ex);
         model.data = "error";
     }
 
-})()
+})();
