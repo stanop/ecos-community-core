4b0aae7fadaa99cb116d519719cefcf13c94416e
Pavel Simonov <pavel.simonov@citeck.ru>
2017-08-31 17:33:36 +0700

    Issue #ECOSCOM-987 - node.get webscript return error when user doesn't have permission

--- ecos-base-core-repo/src/main/resources/alfresco/templates/webscripts/ru/citeck/node/node.get.js
+++ ecos-base-core-repo/src/main/resources/alfresco/templates/webscripts/ru/citeck/node/node.get.js
@@ -1,79 +1,83 @@
-(function() {
+(function () {
 
+    var propsResult = [],
+        assocsResult = [],
+        childAssocsResult = [],
+        childrenResult = [];
 
-if (args['nodeRef']) {
-	var node = search.findNode(args['nodeRef']);
+    if (args['nodeRef']) {
 
-	if (node) {
-		model.node = node;
-		model.siteShortName = node.siteShortName;
+        var node = search.findNode(args['nodeRef']);
 
-		var propsArr 			 = args['props'] ? extractNames(args['props']) : extractKeys(node.properties), 
-				assocsArr 		 = args['assocs'] ? extractNames(args['assocs']) : extractKeys(node.assocs), 
-				childAssocsArr = args['childAssocs'] ? extractNames(args['childAssocs']) : extractKeys(node.childAssocs);
+        if (node) {
+            model.node = node;
+            model.siteShortName = node.siteShortName;
 
-		var propsResult 			= [],
-				assocsResult 			= [],
-				childAssocsResult = [];
-		
-		for (var i = 0; i < propsArr.length; i++) {
-			var prop = propsArr[i];
-			propsResult.push({
-				key: utils.shortQName(prop),
-				value: node.properties[prop]
-			});
-		}
+            var propsArr = args['props'] ? extractNames(args['props']) : extractKeys(node.properties),
+                assocsArr = args['assocs'] ? extractNames(args['assocs']) : extractKeys(node.assocs),
+                childAssocsArr = args['childAssocs'] ? extractNames(args['childAssocs']) : extractKeys(node.childAssocs);
 
-		for (var i = 0; i < assocsArr.length; i++) {
-			var assoc = assocsArr[i];
-			var assocObj = node.assocs[assoc];
-			if (assocObj) {
-				var nodeRefs = [];
-				if (assocObj.length > 0) {
-					for (var j = 0; j < assocObj.length; j++)
-						nodeRefs.push(assocObj[j].nodeRef.toString());
-				}
-				assocsResult.push({
-					key: utils.shortQName(assoc),
-					value: nodeRefs
-				});
-			}
-		}
+            for (var i = 0; i < propsArr.length; i++) {
+                var prop = propsArr[i];
+                propsResult.push({
+                    key: utils.shortQName(prop),
+                    value: node.properties[prop]
+                });
+            }
 
-		for (var i = 0; i < childAssocsArr.length; i++) {
-			var assoc = childAssocsArr[i];
-			var assocObj = node.childAssocs[assoc];
-			if (assocObj) {
-				var nodeRefs = [];
-				if (assocObj.length > 0) {
-					for (var j = 0; j < assocObj.length; j++)
-						nodeRefs.push(assocObj[j].nodeRef.toString());
-				}
-				childAssocsResult.push({
-					key: utils.shortQName(assoc),
-					value: nodeRefs
-				});
-			}
-		}
+            for (var i = 0; i < assocsArr.length; i++) {
+                var assoc = assocsArr[i];
+                var assocObj = node.assocs[assoc];
+                if (assocObj) {
+                    var nodeRefs = [];
+                    if (assocObj.length > 0) {
+                        for (var j = 0; j < assocObj.length; j++)
+                            nodeRefs.push(assocObj[j].nodeRef.toString());
+                    }
+                    assocsResult.push({
+                        key: utils.shortQName(assoc),
+                        value: nodeRefs
+                    });
+                }
+            }
 
-		if (args["children"]) {
-			var onlyChildren = extractNames(args["children"]),
-					onlyChildrenResult = [];
+            for (var i = 0; i < childAssocsArr.length; i++) {
+                var assoc = childAssocsArr[i];
+                var assocObj = node.childAssocs[assoc];
+                if (assocObj) {
+                    var nodeRefs = [];
+                    if (assocObj.length > 0) {
+                        for (var j = 0; j < assocObj.length; j++)
+                            nodeRefs.push(assocObj[j].nodeRef.toString());
+                    }
+                    childAssocsResult.push({
+                        key: utils.shortQName(assoc),
+                        value: nodeRefs
+                    });
+                }
+            }
 
-			for (var c = 0; c < node.children.length; c++) {
-				if (onlyChildren.toString().indexOf(node.children[c].typeShort) >= 0)
-					onlyChildrenResult.push(node.children[c]);
-			}
+            if (args["children"]) {
+                var onlyChildren = extractNames(args["children"]),
+                    onlyChildrenResult = [];
 
-			model.children = onlyChildrenResult;
-		} else { model.children = node.children }
+                for (var c = 0; c < node.children.length; c++) {
+                    if (onlyChildren.toString().indexOf(node.children[c].typeShort) >= 0)
+                        onlyChildrenResult.push(node.children[c]);
+                }
 
-	}
-}
+                childrenResult = onlyChildrenResult;
+            } else {
+                childrenResult = node.children;
+            }
 
-model.props 			= propsResult;
-model.assocs 			= assocsResult;
-model.childAssocs = childAssocsResult;
+        }
+    }
+
+    model.children = childrenResult;
+    model.props = propsResult;
+    model.assocs = assocsResult;
+    model.childAssocs = childAssocsResult;
 
 })();
 
@@ -83,25 +87,27 @@ model.childAssocs = childAssocsResult;
 // ----------------- 
 
 function extractNames(values) {
-	var result = [];
-	if (values) {
-		values = ('' + values).replace(/^\s+|\s+$/g, '');
-		var valuesArr = values.split(',');
-		for (var i = 0; i < valuesArr.length; i++) {
-			var valueName = valuesArr[i].replace(/^\s+|\s+$/g, '');
-			if (valueName)
-				result.push(valueName);
-		}
-	}
-	return result;
-};
+    var result = [];
+    if (values) {
+        values = ('' + values).replace(/^\s+|\s+$/g, '');
+        var valuesArr = values.split(',');
+        for (var i = 0; i < valuesArr.length; i++) {
+            var valueName = valuesArr[i].replace(/^\s+|\s+$/g, '');
+            if (valueName)
+                result.push(valueName);
+        }
+    }
+    return result;
+}
 
 function extractKeys(values) {
-	var result = [];
+    var result = [];
 
-	if (values instanceof Array) {
-		for (var v = 0; v < values.length; v++) result.push(values[v++].typeShort);
-	} else { for (var v in values) result.push(v) }
-	
-	return result;
-};
+    if (values instanceof Array) {
+        for (var v = 0; v < values.length; v++) result.push(values[v++].typeShort);
+    } else {
+        for (var v in values) result.push(v)
+    }
+
+    return result;
+}
