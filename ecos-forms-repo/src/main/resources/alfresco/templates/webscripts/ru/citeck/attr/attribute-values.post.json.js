c654ca5f9711596230a6b028c257dca2eb463468
Pavel Simonov <pavel.simonov@citeck.ru>
2017-09-13 18:35:30 +0700

    Issue #MIGRATION-137 - attribute-values.post.json.js optimization

--- ecos-forms-repo/src/main/resources/alfresco/templates/webscripts/ru/citeck/attr/attribute-values.post.json.js
+++ ecos-forms-repo/src/main/resources/alfresco/templates/webscripts/ru/citeck/attr/attribute-values.post.json.js
@@ -2,48 +2,31 @@
     
     var requests = json.get('requests'),
         nodes = {},
-        persistedAttributes = {},
         values = [];
+
     for(var i = 0, ii = requests.length(); i < ii; i++) {
         var request = requests.get(i),
             nodeRef = request.get('nodeRef'),
             node = (nodes[nodeRef] = nodes[nodeRef] || search.findNode(nodeRef)),
-            attribute = request.get('attribute');
-        
-        if(!node) {
+            attribute = request.get('attribute'),
+            value = node ? attributes.get(node, attribute) : null;
+
+        if (value == null) {
             values.push({
                 nodeRef: nodeRef,
                 attribute: attribute,
                 persisted: false
-            })
-        }
-        
-        if(!persistedAttributes[nodeRef]) {
-            var names = attributes.getPersisted(node),
-                persisted = {};
-            for(var j in names) {
-                persisted[names[j]] = 1;
-            }
-            persistedAttributes[nodeRef] = persisted;
-        }
-        
-        if(persistedAttributes[nodeRef][attribute]) {
-            var value = attributes.get(node, attribute);
+            });
+        } else {
             values.push({
                 nodeRef: nodeRef,
                 attribute: attribute,
                 persisted: true,
                 value: value
-            })
-        } else {
-            values.push({
-                nodeRef: nodeRef,
-                attribute: attribute,
-                persisted: false
-            })
+            });
         }
     }
     
     model.values = values;
     
-})()
+})();
