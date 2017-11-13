e972ed5b0eefd255e28e7aa5b87f29298e06c82a
Oleg Onischuk <oleg.onischuk@citeck.ru>
2017-11-13 21:12:21 +0300

    ECOSCOM-1101 Аmount in words converter

--- ecos-base-core-repo/src/main/java/ru/citeck/ecos/utils/converter/amount/CurrencyRub.java
+++ ecos-base-core-repo/src/main/java/ru/citeck/ecos/utils/converter/amount/CurrencyRub.java
@@ -1,7 +1,5 @@
 package ru.citeck.ecos.utils.converter.amount;
 
-import org.springframework.extensions.surf.util.I18NUtil;
-
 import java.util.Locale;
 
 /**
@@ -15,12 +13,12 @@ class CurrencyRub extends Currency {
 
     @Override
     void initializationResources() {
-        setFractional1(I18NUtil.getMessage("amount-in-word-converter.currency.rub.fractional-1", locale));
-        setFractional2(I18NUtil.getMessage("amount-in-word-converter.currency.rub.fractional-2", locale));
-        setFractional3(I18NUtil.getMessage("amount-in-word-converter.currency.rub.fractional-3", locale));
+        setFractional1(getMessage("amount-in-word-converter.currency.rub.fractional-1"));
+        setFractional2(getMessage("amount-in-word-converter.currency.rub.fractional-2"));
+        setFractional3(getMessage("amount-in-word-converter.currency.rub.fractional-3"));
 
-        setIntact1(I18NUtil.getMessage("amount-in-word-converter.currency.rub.intact-1", locale));
-        setIntact2(I18NUtil.getMessage("amount-in-word-converter.currency.rub.intact-2", locale));
-        setIntact3(I18NUtil.getMessage("amount-in-word-converter.currency.rub.intact-3", locale));
+        setIntact1(getMessage("amount-in-word-converter.currency.rub.intact-1"));
+        setIntact2(getMessage("amount-in-word-converter.currency.rub.intact-2"));
+        setIntact3(getMessage("amount-in-word-converter.currency.rub.intact-3"));
     }
 }
