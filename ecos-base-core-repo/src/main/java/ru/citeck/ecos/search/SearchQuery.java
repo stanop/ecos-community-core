4e7a1265abcee7d5e33a3f3c3c10d77771f79981
Alexey Sokolov <niyan@vtomske.ru>
2017-11-22 16:41:28 +0700

    ECOSCOM-1115 - Implement webscript Batch Search Query

--- ecos-base-core-repo/src/main/java/ru/citeck/ecos/search/SearchQuery.java
+++ ecos-base-core-repo/src/main/java/ru/citeck/ecos/search/SearchQuery.java
@@ -45,7 +45,6 @@ public class SearchQuery {
     @Setter
     private NodeService nodeService;
 
-    private JSONObject defaultSchema;
 
     public JSONObject queryNodes(String searchQuery) throws JSONException {
         return queryNodes(searchQuery, DEFAULT_MAX_ITEMS, DEFAULT_SKIP_COUNT, DEFAULT_LANGUAGE, DEFAULT_SCHEMA);
@@ -131,7 +130,7 @@ public class SearchQuery {
                         @SuppressWarnings("unchecked")
                         List<NodeRef> nodeValues = (List<NodeRef>) nodeValue;
                         if (!nodeValues.isEmpty()) {
-                            JSONObject subNodesSchema = schemaObjValue.length() > 0 ? schemaObjValue : defaultSchema;
+                            JSONObject subNodesSchema = schemaObjValue.length() > 0 ? schemaObjValue : DEFAULT_SCHEMA;
                             value = formatNode(nodeValues.get(0), subNodesSchema);
                         }
                     }
@@ -141,7 +140,7 @@ public class SearchQuery {
                 } else if (schemaValue instanceof JSONArray) {
 
                     JSONArray schemaArray = (JSONArray) schemaValue;
-                    JSONObject subNodesSchema = defaultSchema;
+                    JSONObject subNodesSchema = DEFAULT_SCHEMA;
                     if (schemaArray.length() > 0 && schemaArray.optJSONObject(0).length() > 0) {
                         subNodesSchema = schemaArray.optJSONObject(0);
                     }
