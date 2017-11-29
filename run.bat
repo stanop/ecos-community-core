346b0de4c5a9a5ff81035f8edcfb32cce4b0f3eb
Andrew Timokhin <andrey.timokhin@citeck.ru>
2017-11-29 18:55:13 +0700

    ECOSCOM-1113 - Solve the "Error status 408 Read timed out" problem when changing the status of "availabe" for users with a large number of tasks. Added comment to run.bat

--- run.bat
+++ run.bat
@@ -1,8 +1,10 @@
 @ECHO OFF
 
+REM To run in debug mode, add to the MAVEN_OPTS: -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000
+
 IF "%MAVEN_OPTS%" == "" (
     ECHO The environment variable 'MAVEN_OPTS' is not set, setting it for you
-    SET MAVEN_OPTS=-Xms256m -Xmx2G -XX:PermSize=300m -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000
+    SET MAVEN_OPTS=-Xms256m -Xmx2G -XX:PermSize=300m
 )
 ECHO MAVEN_OPTS is set to '%MAVEN_OPTS%'
 mvn clean install ecos:run -DskipTests=true -Pdevelopment
