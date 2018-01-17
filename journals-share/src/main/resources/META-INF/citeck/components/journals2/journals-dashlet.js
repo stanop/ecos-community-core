95814cdf1a888680fa8904cff6822301070d7e03
Pavel Simonov <pavel.simonov@citeck.ru>
2018-01-17 16:07:11 +0700

    ECOSCOM-1213 - Journals dashlet show an error and does not loaded

--- journals-share/src/main/resources/META-INF/citeck/components/journals2/journals-dashlet.js
+++ journals-share/src/main/resources/META-INF/citeck/components/journals2/journals-dashlet.js
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with Citeck EcoS. If not, see <http://www.gnu.org/licenses/>.
  */
-define(['citeck/utils/knockout.utils', 'citeck/components/journals2/journals'], function(koutils, Journals) {
+define(['jquery', 'citeck/utils/knockout.utils', 'citeck/components/journals2/journals'], function(jq, koutils, Journals) {
 
 var PopupManager = Alfresco.util.PopupManager,
 	koclass = koutils.koclass,
