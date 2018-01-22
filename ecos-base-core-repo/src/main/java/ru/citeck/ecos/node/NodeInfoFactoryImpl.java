f6043952e2588829aefc5c160937b03bf20d6799
Anton Kulikov <anton.kulikov@citeck.ru>
2018-01-22 14:38:39 +0300

    ECOSCOM-1224 - Fix creating a user account

--- ecos-base-core-repo/src/main/java/ru/citeck/ecos/node/NodeInfoFactoryImpl.java
+++ ecos-base-core-repo/src/main/java/ru/citeck/ecos/node/NodeInfoFactoryImpl.java
@@ -24,6 +24,7 @@ import org.alfresco.repo.workflow.WorkflowModel;
 import org.alfresco.service.ServiceRegistry;
 import org.alfresco.service.cmr.dictionary.*;
 import org.alfresco.service.cmr.repository.*;
+import org.alfresco.service.cmr.security.PersonService;
 import org.alfresco.service.cmr.workflow.WorkflowInstance;
 import org.alfresco.service.cmr.workflow.WorkflowTask;
 import org.alfresco.service.namespace.NamespaceService;
@@ -62,6 +63,7 @@ class NodeInfoFactoryImpl implements NodeInfoFactory
 	private MimetypeService mimetypeService;
 	private NodeAttributeService nodeAttributeService;
 	private AssociationIndexing associationIndexing;
+	private PersonService personService;
 	
 	/////////////////////////////////////////////////////////////////
 	//                     GENERAL INTERFACE                       //
@@ -286,9 +288,14 @@ class NodeInfoFactoryImpl implements NodeInfoFactory
 			parentAssocName = QName.createQName(parentAssoc.getNamespaceURI(), GUID.generate());
 		}
 
-		ChildAssociationRef childAssocRef = nodeService.createNode(parent, parentAssoc, parentAssocName, nodeType);
-		
-		nodeRef = childAssocRef.getChildRef();
+        if (nodeType.getPrefixString().equals("cm:person")) {
+			Map<QName, Serializable> properties = nodeInfo.getProperties();
+            nodeRef = personService.createPerson(properties);
+        } else {
+            ChildAssociationRef childAssocRef = nodeService.createNode(parent, parentAssoc, parentAssocName, nodeType);
+            nodeRef = childAssocRef.getChildRef();
+        }
+
 		persist(nodeRef, nodeInfo, full);
 		
 		return nodeRef;
@@ -511,6 +518,10 @@ class NodeInfoFactoryImpl implements NodeInfoFactory
 
     public void setNodeAttributeService(NodeAttributeService nodeAttributeService) {
         this.nodeAttributeService = nodeAttributeService;
+    }
+
+    public void setPersonService(PersonService personService) {
+        this.personService = personService;
     }
 
 	public void init() {
