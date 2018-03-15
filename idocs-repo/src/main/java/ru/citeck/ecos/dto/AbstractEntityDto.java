1536ac5ac5d7aaba67c205ec696883f27f51f736
Dmitriy Konoplev <dmitry.konoplev@citeck.ru>
2018-03-15 09:15:47 +0300

    ECOSCOM-1331 Move completed case management nodes from repo

--- idocs-repo/src/main/java/ru/citeck/ecos/dto/AbstractEntityDto.java
+++ idocs-repo/src/main/java/ru/citeck/ecos/dto/AbstractEntityDto.java
@@ -104,4 +104,17 @@ public class AbstractEntityDto implements Serializable {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntityDto that = (AbstractEntityDto) o;
+        return nodeUUID != null ? nodeUUID.equals(that.nodeUUID) : that.nodeUUID == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return nodeUUID != null ? nodeUUID.hashCode() : 0;
+    }
 }
