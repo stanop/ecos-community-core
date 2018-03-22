23709e077082087478b10e58401516c73a7d58a0
Aysel Seidova <aysel.seidova@citeck.ru>
2018-03-22 17:29:26 +0700

    ECOSCOM-1349 - Load flowable forms by ecos forms - fixed cardlet action for tasks

--- idocs-share/src/main/resources/alfresco/site-webscripts/ru/citeck/components/redirect-to-task/redirect-to-task.get.js
+++ idocs-share/src/main/resources/alfresco/site-webscripts/ru/citeck/components/redirect-to-task/redirect-to-task.get.js
@@ -14,12 +14,16 @@
     if (isCardDetail) {
         redirectURL += "card-details?nodeRef=" + taskDocument;
     } else {
-        var result = remote.call("citeck/invariants/view-check?taskId=" + taskNode.item.node.properties["cm:name"]),
-            formMode = taskStatus == "Completed" ? "view" : "edit",
-            formId = result.exists ? "task-view-edit" : (formMode == "edit" ? "task-edit" : "task-details"),
-            requestParams = "?taskId=" + taskId + "&formMode=" + formMode;
+        var result = remote.call("/citeck/invariants/view-check?taskId=" + taskNode.item.node.properties["cm:name"]);
+        if (result.status == 200) {
+            var response = eval('(' + result + ')'),
+                formMode = taskStatus == "Completed" ? "view" : "edit",
+                formId = response.exists ? "task-view-edit" : (formMode == "edit" ? "task-edit" : "task-details"),
+                requestParams = "?taskId=" + taskId + "&formMode=" + formMode;
 
-        redirectURL += formId + requestParams;
+            redirectURL += formId + requestParams;
+        }
+
     }
 
     status.code = 303;
