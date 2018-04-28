7bbf88e5d7236c1baf1e5129b5ba6a2d0ce7028e
Ilya Kaygorodov <ilya.kaygorodov@citeck.ru>
2018-04-28 12:45:29 +0300

    Fix for filename checking code in ZipUtils

--- idocs-repo/src/main/java/ru/citeck/ecos/utils/ZipUtils.java
+++ idocs-repo/src/main/java/ru/citeck/ecos/utils/ZipUtils.java
@@ -19,7 +19,21 @@ public class ZipUtils {
 
     private static final Charset ENCODING_CP866 = Charset.forName("CP866");
 
-    private static final Pattern ILLEGAL_CHARS_PATTERN = Pattern.compile("[\\n\\r\\t\\f\\\\/?*|`><\"]");
+    private static final Pattern ILLEGAL_CHARS_PATTERN = Pattern.compile(
+            "# Match a valid Windows filename (unspecified file system).          \n" +
+                    "^                                # Anchor to start of string.        \n" +
+                    "(?!                              # Assert filename is not: CON, PRN, \n" +
+                    "  (?:                            # AUX, NUL, COM1, COM2, COM3, COM4, \n" +
+                    "    CON|PRN|AUX|NUL|             # COM5, COM6, COM7, COM8, COM9,     \n" +
+                    "    COM[1-9]|LPT[1-9]            # LPT1, LPT2, LPT3, LPT4, LPT5,     \n" +
+                    "  )                              # LPT6, LPT7, LPT8, and LPT9...     \n" +
+                    "  (?:\\.[^.]*)?                  # followed by optional extension    \n" +
+                    "  $                              # and end of string                 \n" +
+                    ")                                # End negative lookahead assertion. \n" +
+                    "[^<>:\"/\\\\|?*\\x00-\\x1F]*     # Zero or more valid filename chars.\n" +
+                    "[^<>:\"/\\\\|?*\\x00-\\x1F\\ .]  # Last char is not a space or dot.  \n" +
+                    "$                                # Anchor to end of string.            ",
+            Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE | Pattern.COMMENTS);
 
     private static final int FILENAME_MAX_LENGTH = 150;
     private static final int EXTENSION_MAX_LENGTH = 10;
