3f81619e3d580775b467333c21e6a844844a1be1
Alexander Nemerov <alexander.nemerov@citeck.ru>
2018-05-03 12:04:31 +0700

    ECOSCOM-1465 - Webscript for transliteration Simple webscript for transliteration

---
+++ idocs-repo/src/main/resources/alfresco/templates/webscripts/ru/citeck/translit/translit.get.js
@@ -0,0 +1,16 @@
+(function() {
+    var string = args.string;
+
+    var arrru = ['Я','я','Ю','ю','Ч','ч','Ш','ш','Щ','щ','Ж','ж','А','а','Б','б','В','в','Г','г','Д','д','Е','е','Ё','ё','З','з','И','и','Й','й','К','к','Л','л','М','м','Н','н', 'О','о','П','п','Р','р','С','с','Т','т','У','у','Ф','ф','Х','х','Ц','ц','Ы','ы','Ь','ь','Ъ','ъ','Э','э'];
+    var arren = ['Ya','ya','Yu','yu','Ch','ch','Sh','sh','Sh','sh','Zh','zh','A','a','B','b','V','v','G','g','D','d','E','e','E','e','Z','z','I','i','J','j','K','k','L','l','M','m','N','n', 'O','o','P','p','R','r','S','s','T','t','U','u','F','f','H','h','C','c','Y','y','`','`','\'','\'','E', 'e'];
+
+    function cyrillToLatin(text){
+        for(var i=0; i<arrru.length; i++){
+            var reg = new RegExp(arrru[i], "g");
+            text = text.replace(reg, arren[i]);
+        }
+        return text;
+    }
+
+    model.result = cyrillToLatin(string);
+})();
