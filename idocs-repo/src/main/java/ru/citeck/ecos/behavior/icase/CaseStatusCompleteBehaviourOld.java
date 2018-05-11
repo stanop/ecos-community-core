72475cdba9c17e7b7aa1f31ada1ff1ffec153b37
Pavel Simonov <pavel.simonov@citeck.ru>
2018-05-11 11:06:49 +0700

    ECOSCOM-1472 - [Optimization] Upload new version of case

--- idocs-repo/src/main/java/ru/citeck/ecos/behavior/icase/CaseStatusCompleteBehaviourOld.java
+++ idocs-repo/src/main/java/ru/citeck/ecos/behavior/icase/CaseStatusCompleteBehaviourOld.java
@@ -1,31 +1,33 @@
 package ru.citeck.ecos.behavior.icase;
 
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
 import org.alfresco.repo.policy.Behaviour;
 import ru.citeck.ecos.behavior.OrderedBehaviour;
 import org.alfresco.repo.policy.PolicyComponent;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.cmr.repository.StoreRef;
+import org.alfresco.service.cmr.search.ResultSet;
+import org.alfresco.service.cmr.search.SearchParameters;
 import org.alfresco.service.cmr.search.SearchService;
 import org.alfresco.service.namespace.QName;
 import ru.citeck.ecos.icase.CaseStatusPolicies;
 import ru.citeck.ecos.model.ICaseModel;
 import ru.citeck.ecos.model.IdocsFinalStatusModel;
 import ru.citeck.ecos.model.IdocsModel;
-import ru.citeck.ecos.search.ftsquery.FTSQuery;
-import ru.citeck.ecos.utils.NodeUtils;
+import ru.citeck.ecos.utils.RepoUtils;
 
 import java.util.List;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 /**
  * Case status complete behavior
  */
-public class CaseStatusCompleteBehaviour implements CaseStatusPolicies.OnCaseStatusChangedPolicy {
+@Deprecated
+public class CaseStatusCompleteBehaviourOld implements CaseStatusPolicies.OnCaseStatusChangedPolicy {
+
+    /**
+     * Store reference
+     */
+    private StoreRef storeRef;
 
     /**
      * Policy component
@@ -42,25 +44,15 @@ public class CaseStatusCompleteBehaviour implements CaseStatusPolicies.OnCaseSta
      */
     private SearchService searchService;
 
-    private NodeUtils nodeUtils;
-
-    private LoadingCache<QName, Set<NodeRef>> finalStatusesByType;
-
-    private long cacheAge = 300;
-
     /**
      * Init method
      */
     public void init() {
+        storeRef = StoreRef.STORE_REF_WORKSPACE_SPACESSTORE;
         policyComponent.bindClassBehaviour(
                 CaseStatusPolicies.OnCaseStatusChangedPolicy.QNAME, ICaseModel.TYPE_CASE_STATUS,
-                new OrderedBehaviour(this, "onCaseStatusChanged",
-                                     Behaviour.NotificationFrequency.TRANSACTION_COMMIT, 80)
+                new OrderedBehaviour(this, "onCaseStatusChanged", Behaviour.NotificationFrequency.TRANSACTION_COMMIT, 80)
         );
-        finalStatusesByType = CacheBuilder.newBuilder()
-                                          .expireAfterWrite(cacheAge, TimeUnit.SECONDS)
-                                          .maximumSize(100)
-                                          .build(CacheLoader.from(this::getFinalStatuses));
     }
 
     /**
@@ -83,10 +75,21 @@ public class CaseStatusCompleteBehaviour implements CaseStatusPolicies.OnCaseSta
      * @return Check result
      */
     private boolean isAppropriateDocTypeAndStatus(NodeRef documentRef, NodeRef statusRef) {
-        if (documentRef != null && statusRef != null) {
-            QName type = nodeService.getType(documentRef);
-            return finalStatusesByType.getUnchecked(type).contains(statusRef);
+        if (documentRef == null || statusRef == null) {
+            return false;
         }
+
+        List<NodeRef> finalStatusesRefs = getFinalStatuses(nodeService.getType(documentRef));
+
+        for (NodeRef finalStatusRef : finalStatusesRefs) {
+            List<NodeRef> caseStatuses = RepoUtils.getTargetAssoc(finalStatusRef,
+                    IdocsFinalStatusModel.ASSOC_FINAL_STATUSES, nodeService);
+
+            if (caseStatuses != null && caseStatuses.contains(statusRef)) {
+                return true;
+            }
+        }
+
         return false;
     }
 
@@ -95,19 +98,15 @@ public class CaseStatusCompleteBehaviour implements CaseStatusPolicies.OnCaseSta
      * @param documentType Document type
      * @return List of final statuses
      */
-    private Set<NodeRef> getFinalStatuses(QName documentType) {
-        return FTSQuery.create()
-                       .type(IdocsFinalStatusModel.TYPE_DOC_FINAL_STATUS).and()
-                       .exact(IdocsFinalStatusModel.PROP_DOC_TYPE, documentType)
-                       .transactional()
-                       .query(searchService)
-                       .stream()
-                       .flatMap(ref -> getFinalStatuses(ref).stream())
-                       .collect(Collectors.toSet());
-    }
-
-    private List<NodeRef> getFinalStatuses(NodeRef nodeRef) {
-        return nodeUtils.getAssocTargets(nodeRef, IdocsFinalStatusModel.ASSOC_FINAL_STATUSES);
+    private List<NodeRef> getFinalStatuses(QName documentType) {
+        SearchParameters parameters = new SearchParameters();
+        parameters.addStore(storeRef);
+        parameters.setLanguage(SearchService.LANGUAGE_LUCENE);
+        parameters.setQuery("TYPE:\"idocs:documentFinalStatus\" AND " +
+                "@idocs\\:documentType:\"" + documentType + "\"");
+        parameters.addSort("@cm:created", true);
+        ResultSet result = searchService.query(parameters);
+        return result.getNodeRefs();
     }
 
     /** Setters */
@@ -123,17 +122,4 @@ public class CaseStatusCompleteBehaviour implements CaseStatusPolicies.OnCaseSta
     public void setSearchService(SearchService searchService) {
         this.searchService = searchService;
     }
-
-    public void setCacheAge(long cacheAge) {
-        this.cacheAge = cacheAge;
-    }
-
-    public void resetCache() {
-        finalStatusesByType.invalidateAll();
-    }
-
-    public void setNodeUtils(NodeUtils nodeUtils) {
-        this.nodeUtils = nodeUtils;
-    }
 }
-
