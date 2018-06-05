70bb8167a9be299130f4f33153b381644a946941
Dmitriy Konoplev <dmitry.konoplev@citeck.ru>
2018-06-05 11:02:09 +0300

    ECOSCOM-1536 Flowable Form Service - make always trusted certificate

--- ecos-flowable-repo/src/main/java/ru/citeck/ecos/flowable/services/rest/RestFormService.java
+++ ecos-flowable-repo/src/main/java/ru/citeck/ecos/flowable/services/rest/RestFormService.java
@@ -4,8 +4,13 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.flowable.form.model.SimpleFormModel;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Service;
@@ -13,6 +18,12 @@ import org.springframework.web.client.RestTemplate;
 import ru.citeck.ecos.http.BasicAuthInterceptor;
 
 import javax.annotation.PostConstruct;
+import javax.net.ssl.SSLContext;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -57,13 +68,58 @@ public class RestFormService {
         List<HttpMessageConverter<?>> converters = new ArrayList<>();
         converters.add(converter);
 
-        restTemplate = new RestTemplate(converters);
+        HttpComponentsClientHttpRequestFactory factory = createHttpFactory();
+        if (factory == null) {
+            restTemplate = new RestTemplate(converters);
+        } else {
+            restTemplate = new RestTemplate(factory);
+            restTemplate.setMessageConverters(converters);
+        }
+
         restTemplate.getInterceptors().add(new BasicAuthInterceptor(username, password));
 
         baseUrl = restApiUrl.endsWith("/") ? restApiUrl : restApiUrl + "/";
         baseUrl += "form-api/form-repository/";
 
         initialized = true;
+    }
+
+    private HttpComponentsClientHttpRequestFactory createHttpFactory() {
+        /** Disable ssl verification */
+        TrustStrategy acceptingTrustStrategy = new TrustStrategy() {
+            @Override
+            public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                return true;
+            }
+        };
+
+        SSLContext sslContext = null;
+        try {
+            sslContext = org.apache.http.ssl.SSLContexts.custom()
+                    .loadTrustMaterial(null, acceptingTrustStrategy)
+                    .build();
+        } catch (NoSuchAlgorithmException e) {
+            logger.error(e.getMessage(), e);
+            return null;
+        } catch (KeyManagementException e) {
+            logger.error(e.getMessage(), e);
+            return null;
+        } catch (KeyStoreException e) {
+            logger.error(e.getMessage(), e);
+            return null;
+        }
+
+        SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLSocketFactory(csf)
+                .build();
+
+        HttpComponentsClientHttpRequestFactory requestFactory =
+                new HttpComponentsClientHttpRequestFactory();
+
+        requestFactory.setHttpClient(httpClient);
+        return requestFactory;
     }
 
     public Optional<SimpleFormModel> getFormByKey(String formKey) {
