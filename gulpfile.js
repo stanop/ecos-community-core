95cd290fda7b180ceba253e2f645792b9ccd5458
Pavel Simonov <pavel.simonov@citeck.ru>
2018-06-06 10:13:01 +0700

    ECOSCOM-1437 - [Optimization] Fix card-details loading time
    switch all modules to gulp processing

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,21 @@ let gulp = require('gulp'),
     sourcemaps = require('gulp-sourcemaps'),
     rename = require('gulp-rename'),
     uglify = require('gulp-uglify'),
-    cleanCSS = require('gulp-clean-css');
+    cleanCSS = require('gulp-clean-css'),
+    gutil = require('gulp-util');
 
-let moduleId = process.argv[3].substring(2);
+let buildParams = {};
+for (let arg of process.argv) {
+    if (arg.startsWith('--')) {
+        let keyValue = arg.split('=');
+        if (keyValue.length == 2) {
+            buildParams[keyValue[0].substring(2)] = keyValue[1];
+        }
+    }
+}
+
+let moduleId = buildParams['artifactId'];
+let skipCompression = buildParams['skipCompression'] == 'true';
 
 let webRoot = moduleId + "/target/classes/META-INF/";
 let source = {
@@ -14,6 +26,11 @@ let source = {
     ],
     js: [
         webRoot + '**/*.js',
+        '!' + webRoot + 'js/**',
+        '!' + webRoot + '**/*min.js',
+    ],
+    babelJs: [
+        webRoot + 'js/**/*.js',
         '!' + webRoot + '**/*min.js',
     ],
     css: [
@@ -22,13 +39,16 @@ let source = {
     ]
 };
 
-gulp.task('process-css', function() {
-    return gulp.src(source.css)
-        .pipe(sourcemaps.init())
-        .pipe(cleanCSS({
-            inline: false,
-            rebase: false
-        }))
+function processMinResources(paths, processors) {
+
+    let stream = gulp.src(paths)
+        .pipe(sourcemaps.init());
+
+    for (let proc of processors) {
+        stream = stream.pipe(proc);
+    }
+
+    return stream
         .pipe(rename(function (path) {
             path.basename += ".min";
         }))
@@ -36,49 +56,50 @@ gulp.task('process-css', function() {
         .pipe(gulp.dest(function(file) {
             return file.base;
         }));
+}
+
+gulp.task('process-css', function() {
+    return processMinResources(source.css, [
+        skipCompression ? gutil.noop() : cleanCSS({
+            inline: false,
+            rebase: false
+        })
+    ]);
 });
 
 gulp.task('process-jsx', function() {
-    return gulp.src(source.jsx)
-        .pipe(sourcemaps.init())
-        .pipe(babel({
-            compact: true,
-            comments: false,
+    return processMinResources(source.jsx, [
+        babel({
+            compact: false,
             presets: [
                 ['env', {
                     modules: 'amd'
                 }],
                 'react'
             ]
-        }))
-        .pipe(rename(function (path) {
-            path.basename += ".min";
-        }))
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest(function(file) {
-            return file.base;
-        }));
+        }),
+        skipCompression ? gutil.noop() : uglify()
+    ]);
 });
 
-gulp.task('process-js', function() {
-    return gulp.src(source.js)
-        .pipe(sourcemaps.init())
-        .pipe(babel({
+gulp.task('process-babel-js', function() {
+    return processMinResources(source.babelJs, [
+        babel({
             compact: false,
             presets: [
                 ['env', {
                     modules: false
                 }]
             ]
-        }))
-        .pipe(uglify())
-        .pipe(rename(function (path) {
-            path.basename += "-min";
-        }))
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest(function(file) {
-            return file.base;
-        }));
+        }),
+        skipCompression ? gutil.noop() : uglify()
+    ]);
 });
 
-gulp.task('default', ['process-css', 'process-jsx', 'process-js']);
+gulp.task('process-js', function() {
+    return processMinResources(source.js, [
+        skipCompression ? gutil.noop() : uglify()
+    ]);
+});
+
+gulp.task('default', ['process-css', 'process-jsx', 'process-js', 'process-babel-js']);
