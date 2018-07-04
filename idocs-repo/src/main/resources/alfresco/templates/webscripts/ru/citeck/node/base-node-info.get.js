125bcec5f2efe5c25373297dda60833f17155b47
Pavel Simonov <pavel.simonov@citeck.ru>
2018-07-04 14:03:13 +0700

    ECOSCOM-1437 - [Optimization] Fix card-details loading time
    * fix cardlets query cache

---
+++ idocs-repo/src/main/resources/alfresco/templates/webscripts/ru/citeck/node/base-node-info.get.js
@@ -0,0 +1,26 @@
+
+function main() {
+
+    var nodeRef = args['nodeRef'];
+
+    if (nodeRef) {
+
+        var node = search.findNode(nodeRef);
+
+        var updateUtil = services.get("ecos.itemsUpdateState");
+
+        model.result = {
+            permissions: {
+                Read: node.hasPermission('Read'),
+                Write: node.hasPermission('Write')
+            },
+            modified: node.properties['cm:modified'],
+            pendingUpdate: updateUtil.isPendingUpdate(node.nodeRef)
+        }
+
+    } else {
+        throw "nodeRef is a mandatory parameter";
+    }
+}
+
+main();
