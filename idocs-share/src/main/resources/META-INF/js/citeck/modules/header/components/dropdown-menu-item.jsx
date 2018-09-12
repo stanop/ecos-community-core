b13dc2cbc6aa2375b98fa7e5405e06f884a2e881
Ivan Tkachenko <ivan.tkachenko@citeck.ru>
2018-09-12 17:42:40 +0300

    ECOSCOM-1646 - React Menu
    use recompose

--- idocs-share/src/main/resources/META-INF/js/citeck/modules/header/components/dropdown-menu-item.jsx
+++ idocs-share/src/main/resources/META-INF/js/citeck/modules/header/components/dropdown-menu-item.jsx
@@ -1,25 +1,20 @@
-import React, { PureComponent } from "react";
+import React from "react";
+import { pure } from 'recompose';
 import { MenuItem, Image } from 'react-bootstrap';
+import "xstyle!js/citeck/modules/header/components/dropdown-menu-item.css";
 
-import "xstyle!js/citeck/modules/header/base/dropdown-menu-item.css";
+const DropDownMenuItem = ({id, key, targetUrl, image, icon, label, clickEvent, targetUrlType, targetUrlLocation}) => (
+    <MenuItem
+        eventKey={key}
+        href={targetUrl}
+        id={id}
+        className="dropdown-menu-item"
+        onClick={eval('(' + clickEvent + ')')}
+    >
+        {image && <Image src={image} thumbnail />}
+        {icon && <i className={"fa " + icon} />}
+        {label && Alfresco.util.message(label)}
+    </MenuItem>
+);
 
-class DropDownMenuItem extends PureComponent {
-
-    render() {
-
-        const {id, key, targetUrl, image, icon, label, clickEvent, targetUrlType, targetUrlLocation} = this.props;
-
-        return (
-            <MenuItem eventKey={key}
-                      href={targetUrl}
-                      id={id}
-                      className="dropdown-menu-item"
-                      onClick={eval('(' + clickEvent + ')')}>
-                {image && <Image src={image} thumbnail />}
-                {icon && <i className={"fa " + icon} />}
-                {label && Alfresco.util.message(label)}
-            </MenuItem>
-        );
-    }
-}
-export default DropDownMenuItem;
+export default pure(DropDownMenuItem);
--- idocs-share/src/main/resources/META-INF/js/citeck/modules/header/components/search.jsx
+++ idocs-share/src/main/resources/META-INF/js/citeck/modules/header/components/search.jsx
@@ -1,59 +1,61 @@
 import React from 'react';
+import { compose, withProps, withState, withHandlers } from 'recompose';
 import SearchDropdown from 'js/citeck/modules/header/components/search-dropdown';
 import "xstyle!js/citeck/modules/header/components/search.css";
 
-export default class Search extends React.Component {
-    state = {
-        searchText: ""
-    };
-
-    onKeyDown = (e) => {
-        if (e.key === 'Enter') {
-            let url = this.props.searchPageUrl || "dp/ws/faceted-search#searchTerm=" + this.generateSearchTerm(this.state.searchText) + "&scope=repo";
-            window.location = Alfresco.constants.URL_PAGECONTEXT + url;
-        }
-    };
-
-    generateSearchTerm = (terms) => {
-        let searchTerm = this.props.hiddenSearchTerms ? "(" + terms + ") " + this.props.hiddenSearchTerms : terms;
-
-        return encodeURIComponent(searchTerm);
-    };
-
-    onTextChange = (e) => {
-        this.setState({searchText: e.target.value});
-    };
-
-    onSearchClearClick = () => {
-        this.setState({searchText: ""});
-    };
-
-    render() {
-        const { placeholder } = this.props;
-        const searchPlaceholder = placeholder ? Alfresco.util.message(placeholder) : Alfresco.util.message("Search files, people, sites");
-
-        return (
-            <div className="alfresco-header-SearchBox share-header-search" id="HEADER_SEARCH_BOX" >
-                <div className="alfresco-header-SearchBox-inner share-header-search__inner">
-                    <SearchDropdown />
-                    <input
-                        id="HEADER_SEARCHBOX_FORM_FIELD"
-                        className="alfresco-header-SearchBox-text"
-                        type="text"
-                        placeholder={searchPlaceholder}
-                        onChange={this.onTextChange}
-                        value={this.state.searchText}
-                        onKeyDown={this.onKeyDown}
-                    />
-                    <div
-                        className="share-header-search__clear-button"
-                        title={Alfresco.util.message("Clear")} // TODO set correct message
-                        onClick={this.onSearchClearClick}
-                    >
-                        <i className={"fa fa-times-circle"} />
-                    </div>
-                </div>
+const Search = ({ searchText, onTextChange, onKeyDown, onSearchClearClick, searchPlaceholder, clearButtonTitle }) => (
+    <div id="HEADER_SEARCH_BOX" className="alfresco-header-SearchBox share-header-search">
+        <div className="alfresco-header-SearchBox-inner share-header-search__inner">
+            <SearchDropdown />
+            <input
+                id="HEADER_SEARCHBOX_FORM_FIELD"
+                className="alfresco-header-SearchBox-text"
+                type="text"
+                placeholder={searchPlaceholder}
+                onChange={onTextChange}
+                value={searchText}
+                onKeyDown={onKeyDown}
+            />
+            <div
+                className="share-header-search__clear-button"
+                title={clearButtonTitle}
+                onClick={onSearchClearClick}
+            >
+                <i className={"fa fa-times-circle"} />
             </div>
-        );
-    }
-}
+        </div>
+    </div>
+);
+
+const generateSearchTerm = (terms, hiddenSearchTerms) => {
+    let searchTerm = hiddenSearchTerms ? "(" + terms + ") " + hiddenSearchTerms : terms;
+
+    return encodeURIComponent(searchTerm);
+};
+
+const enhance = compose(
+    withProps(props => {
+        return {
+            searchPlaceholder: props.placeholder ? window.Alfresco.util.message(props.placeholder) : window.Alfresco.util.message("Search files, people, sites"),
+            clearButtonTitle: window.Alfresco.util.message("Clear") // TODO set correct message
+        }
+    }),
+    withState('searchText', 'updateSearchText', ''),
+    withHandlers({
+        onKeyDown: props => e => {
+            const { searchPageUrl, searchText, hiddenSearchTerms } = props;
+            if (e.key === 'Enter') {
+                let url = searchPageUrl || "dp/ws/faceted-search#searchTerm=" + generateSearchTerm(searchText, hiddenSearchTerms) + "&scope=repo";
+                window.location = window.Alfresco.constants.URL_PAGECONTEXT + url;
+            }
+        },
+        onSearchClearClick: props => () => {
+            props.updateSearchText("");
+        },
+        onTextChange: props => e => {
+            props.updateSearchText(e.target.value);
+        }
+    })
+);
+
+export default enhance(Search);
