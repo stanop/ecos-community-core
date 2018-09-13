2e0e305989019d6a9e599515822ed9625e43f9f1
Lev Bavshin <lev.bavshin@citeck.ru>
2018-09-13 16:49:45 +0700

    ECOSCOM-1688 - Approval tasks through e-mail (grafted from 5710e3f59a691d243e10fa82de2217ef44e52348)

--- 1st-override-repo/src/main/java/ru/citeck/ecos/model/BpmModel.java
+++ 1st-override-repo/src/main/java/ru/citeck/ecos/model/BpmModel.java
@@ -9,6 +9,7 @@ public class BpmModel {
     public static final QName PROPERTY_TASK_ID = QName.createQName(BPM_NAMESPASE, "taskId");
     public static final QName PROPERTY_COMMENT = QName.createQName(BPM_NAMESPASE, "comment");
     public static final QName PROPERTY_DESCRIPTION = QName.createQName(BPM_NAMESPASE, "description");
+    public static final QName PROPERTY_STATUS = QName.createQName(BPM_NAMESPASE, "status");
 
     public static final QName TYPE_APPROVAL_TOKEN = QName.createQName(BPM_NAMESPASE, "approvalToken");
     public static final QName PROPERTY_TOKEN_VALUE = QName.createQName(BPM_NAMESPASE, "tokenValue");
