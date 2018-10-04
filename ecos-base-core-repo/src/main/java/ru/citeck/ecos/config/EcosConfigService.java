72600a7b52d1bd516daa07bd1824cb0fe4741223
Pavel Simonov <pavel.simonov@citeck.ru>
2018-10-04 12:46:13 +0700

    ECOSCOM-1339 - [Optimization] Reduce queries to solr for type config:ecosConfig

--- ecos-base-core-repo/src/main/java/ru/citeck/ecos/config/EcosConfigService.java
+++ ecos-base-core-repo/src/main/java/ru/citeck/ecos/config/EcosConfigService.java
@@ -3,15 +3,21 @@ package ru.citeck.ecos.config;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+import org.alfresco.model.ContentModel;
 import org.alfresco.repo.security.authentication.AuthenticationUtil;
+import org.alfresco.service.cmr.repository.ChildAssociationRef;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
-import org.alfresco.service.cmr.search.*;
+import org.alfresco.service.cmr.search.SearchService;
+import org.alfresco.service.namespace.QName;
+import org.alfresco.util.ParameterCheck;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import ru.citeck.ecos.model.ConfigModel;
-import ru.citeck.ecos.search.ftsquery.FTSQuery;
 
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
@@ -23,14 +29,15 @@ public class EcosConfigService {
 
     private static final Log logger = LogFactory.getLog(EcosConfigService.class);
 
-    private SearchService searchService;
+    private static final NodeRef CONFIGS_ROOT = new NodeRef("workspace://SpacesStore/ecos-config-root");
+
     private NodeService nodeService;
 
     private LoadingCache<String, Optional<NodeRef>> configRefByKey;
 
     public EcosConfigService() {
         configRefByKey = CacheBuilder.newBuilder()
-                                     .expireAfterWrite(300, TimeUnit.SECONDS)
+                                     .expireAfterAccess(30, TimeUnit.SECONDS)
                                      .maximumSize(200)
                                      .build(CacheLoader.from(this::findConfigRef));
     }
@@ -44,8 +51,7 @@ public class EcosConfigService {
     public Object getParamValue(final String key) {
         return AuthenticationUtil.runAsSystem(() -> {
             Optional<NodeRef> config = getConfigRef(key);
-            return config.map(ref -> nodeService.getProperty(ref, ConfigModel.PROP_VALUE))
-                         .orElse(null);
+            return config.map(ref -> nodeService.getProperty(ref, ConfigModel.PROP_VALUE)).orElse(null);
         });
     }
 
@@ -87,6 +93,28 @@ public class EcosConfigService {
         });
     }
 
+    public NodeRef createConfig(Map<QName, Serializable> properties) {
+
+        String configKey = (String) properties.get(ConfigModel.PROP_KEY);
+        ParameterCheck.mandatory(ConfigModel.PROP_KEY.getLocalName(), configKey);
+
+        Optional<NodeRef> configRef = getConfigRef(configKey);
+        if (configRef.isPresent()) {
+            return configRef.get();
+        }
+
+        QName assocQName = QName.createQName(ConfigModel.NAMESPACE, configKey);
+        ChildAssociationRef configRefAssoc = nodeService.createNode(CONFIGS_ROOT,
+                                                                    ContentModel.ASSOC_CONTAINS,
+                                                                    assocQName,
+                                                                    ConfigModel.TYPE_ECOS_CONFIG,
+                                                                    properties);
+
+        configRefByKey.invalidate(configKey);
+
+        return configRefAssoc.getChildRef();
+    }
+
     /**
      * Set a config value. Config node is searched through children of {@code rootPath} with
      * type {@code ConfigModel.TYPE_ECOS_CONFIG}.
@@ -106,7 +134,7 @@ public class EcosConfigService {
         setValue(key, value);
     }
 
-    private Optional<NodeRef> getConfigRef(String key) {
+    public Optional<NodeRef> getConfigRef(String key) {
         Optional<NodeRef> configRef = configRefByKey.getUnchecked(key);
         if (!configRef.isPresent() || !nodeService.exists(configRef.get())) {
             configRefByKey.invalidate(key);
@@ -116,25 +144,23 @@ public class EcosConfigService {
     }
 
     private Optional<NodeRef> findConfigRef(String key) {
-        return AuthenticationUtil.runAsSystem(() ->
-                FTSQuery.create()
-                        .type(ConfigModel.TYPE_ECOS_CONFIG).and()
-                        .exact(ConfigModel.PROP_KEY, key)
-                        .transactional()
-                        .queryOne(searchService)
-        );
+        return AuthenticationUtil.runAsSystem(() -> {
+            List<ChildAssociationRef> configs;
+            configs = nodeService.getChildAssocsByPropertyValue(CONFIGS_ROOT, ConfigModel.PROP_KEY, key);
+            return configs.stream().map(ChildAssociationRef::getChildRef).findFirst();
+        });
     }
 
     public void clearCache() {
         configRefByKey.invalidateAll();
     }
 
-    public void setSearchService(SearchService searchService) {
-        this.searchService = searchService;
-    }
-
     public void setNodeService(NodeService nodeService) {
         this.nodeService = nodeService;
+    }
+
+    public NodeRef getConfigsRoot() {
+        return CONFIGS_ROOT;
     }
 
     private class NoSuchConfigException extends Exception {
---
+++ ecos-base-core-repo/src/main/java/ru/citeck/ecos/config/patch/MoveConfigsToRoot.java
@@ -0,0 +1,172 @@
+package ru.citeck.ecos.config.patch;
+
+import org.alfresco.model.ContentModel;
+import org.alfresco.repo.module.AbstractModuleComponent;
+import org.alfresco.repo.transaction.AlfrescoTransactionSupport;
+import org.alfresco.repo.transaction.RetryingTransactionHelper;
+import org.alfresco.service.ServiceRegistry;
+import org.alfresco.service.cmr.repository.ChildAssociationRef;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.cmr.search.QueryConsistency;
+import org.alfresco.service.cmr.search.SearchService;
+import org.alfresco.service.transaction.TransactionService;
+import org.alfresco.util.transaction.TransactionListenerAdapter;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.citeck.ecos.action.group.GroupActionConfig;
+import ru.citeck.ecos.action.group.GroupActionService;
+import ru.citeck.ecos.config.EcosConfigService;
+import ru.citeck.ecos.model.ConfigModel;
+import ru.citeck.ecos.records.RecordRef;
+import ru.citeck.ecos.records.RecordsService;
+import ru.citeck.ecos.records.RecordsUtils;
+import ru.citeck.ecos.records.query.RecordsQuery;
+
+import java.io.Serializable;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MoveConfigsToRoot extends AbstractModuleComponent {
+
+    private static final Log logger = LogFactory.getLog(MoveConfigsToRoot.class);
+
+    private GroupActionService groupActionService;
+    private TransactionService transactionService;
+    private EcosConfigService ecosConfigService;
+    private RecordsService recordsService;
+    private NodeService nodeService;
+
+    @Override
+    protected void executeInternal() throws Exception {
+
+        RetryingTransactionHelper txnHelper = transactionService.getRetryingTransactionHelper();
+
+        AlfrescoTransactionSupport.bindListener(new TransactionListenerAdapter() {
+            @Override
+            public void afterCommit() {
+                txnHelper.doInTransaction(() -> updateConfigs());
+            }
+        });
+    }
+
+    public boolean updateConfigs() {
+
+        RecordsQuery query = new RecordsQuery();
+        query.setLanguage(SearchService.LANGUAGE_CMIS_STRICT);
+        query.setConsistency(QueryConsistency.TRANSACTIONAL);
+        query.setQuery("select * from config:ecosConfig");
+        Iterable<RecordRef> ecosConfigs = recordsService.getIterableRecords(query);
+
+        GroupActionConfig config = new GroupActionConfig();
+        config.setBatchSize(10);
+        config.setAsync(false);
+        config.setMaxResults(0);
+        config.setMaxErrors(0);
+
+        logger.info("Start " + getClass());
+
+        AtomicInteger fixed = new AtomicInteger(0);
+        AtomicInteger skipped = new AtomicInteger(0);
+        AtomicInteger total = new AtomicInteger(0);
+
+        NodeRef configsRoot = ecosConfigService.getConfigsRoot();
+
+        groupActionService.execute(ecosConfigs, recordRef -> {
+
+            total.incrementAndGet();
+
+            NodeRef configRef = RecordsUtils.toNodeRef(recordRef);
+            if (!nodeService.exists(configRef)) {
+                logger.error("Config node doesn't exists: " + recordRef);
+                return;
+            }
+
+            ChildAssociationRef parentAssoc = nodeService.getPrimaryParent(configRef);
+            if (!configsRoot.equals(parentAssoc.getParentRef())) {
+
+                String key = (String) nodeService.getProperty(configRef, ConfigModel.PROP_KEY);
+
+                Optional<NodeRef> configInRoot = ecosConfigService.getConfigRef(key);
+
+                if (configInRoot.isPresent()) {
+
+                    Serializable internalValue = nodeService.getProperty(configInRoot.get(), ConfigModel.PROP_VALUE);
+                    Serializable externalValue = nodeService.getProperty(configRef, ConfigModel.PROP_VALUE);
+
+                    if (!Objects.equals(internalValue, externalValue)) {
+                        logger.info("Change config value of " + configInRoot.get() +
+                                " from " + internalValue + " to " + externalValue);
+
+                        nodeService.setProperty(configRef, ConfigModel.PROP_VALUE, externalValue);
+                    }
+
+                    logger.info("Delete unnecessary config node: " + configRef +
+                                " with key " + key + " and value " + externalValue);
+                    nodeService.deleteNode(configRef);
+
+                } else {
+
+                    String baseName = (String) nodeService.getProperty(configRef, ContentModel.PROP_NAME);
+                    String name = baseName;
+
+                    NodeRef configWithSameName = nodeService.getChildByName(configsRoot,
+                            ContentModel.ASSOC_CONTAINS,
+                            name);
+                    int idx = 1;
+                    while (configWithSameName != null) {
+                        name = baseName + " (" + idx + ")";
+                        configWithSameName = nodeService.getChildByName(configsRoot,
+                                ContentModel.ASSOC_CONTAINS,
+                                name);
+                        idx++;
+                    }
+
+                    if (!baseName.equals(name)) {
+                        nodeService.moveNode(configRef,
+                                configsRoot,
+                                ContentModel.ASSOC_CONTAINS,
+                                parentAssoc.getQName());
+                    }
+                }
+                fixed.incrementAndGet();
+            } else {
+                skipped.incrementAndGet();
+            }
+        }, config);
+
+        logger.info("Stop " + getClass() +
+                    ". Fixed: " + fixed.get() +
+                    " Skipped: " + skipped.get() +
+                    " Total: " + total);
+
+        return true;
+    }
+
+    @Autowired
+    public void setEcosConfigService(EcosConfigService ecosConfigService) {
+        this.ecosConfigService = ecosConfigService;
+    }
+
+    @Autowired
+    public void setGroupActionService(GroupActionService groupActionService) {
+        this.groupActionService = groupActionService;
+    }
+
+    @Autowired
+    public void setRecordsService(RecordsService recordsService) {
+        this.recordsService = recordsService;
+    }
+
+    @Autowired
+    public void setTransactionService(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @Autowired
+    public void setServiceRegistry(ServiceRegistry serviceRegistry) {
+        super.setServiceRegistry(serviceRegistry);
+        nodeService = serviceRegistry.getNodeService();
+    }
+}
