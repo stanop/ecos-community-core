84dc9caf1b5e4b3adda990ae699f5aef570a46ad
Oleg Onischuk <oleg.onischuk@citeck.ru>
2018-11-28 23:37:08 +0300

    ECOSCOM-1801 - Develop menu service items count cache menuConfig priority

--- idocs-repo/src/main/java/ru/citeck/ecos/menu/resolvers/AbstractJournalsResolver.java
+++ idocs-repo/src/main/java/ru/citeck/ecos/menu/resolvers/AbstractJournalsResolver.java
@@ -1,6 +1,10 @@
 package ru.citeck.ecos.menu.resolvers;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import org.alfresco.model.ContentModel;
+import org.alfresco.repo.security.authentication.AuthenticationUtil;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.namespace.NamespaceService;
 import org.alfresco.service.namespace.QName;
@@ -17,17 +21,15 @@ import ru.citeck.ecos.records.RecordRef;
 import ru.citeck.ecos.records.query.RecordsResult;
 import ru.citeck.ecos.utils.RepoUtils;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public abstract class AbstractJournalsResolver extends AbstractMenuItemsResolver {
 
     protected static final String LIST_ID_KEY = "listId";
     private static final String JOURNAL_REF_KEY = "journalRef";
     private static final String JOURNAL_LINK_KEY = "JOURNAL_LINK";
-    private static final Integer COUNT_MAX = Integer.MAX_VALUE;
+    private static final Integer COUNT_MAX = 0;
     private static final String CRIT_ELEM_FORMAT = "\"%s_%d\":\"%s\"";
     private static final JGqlPageInfoInput PAGE_INFO = new JGqlPageInfoInput(null, COUNT_MAX,
             Collections.emptyList(), 0);
@@ -35,6 +37,23 @@ public abstract class AbstractJournalsResolver extends AbstractMenuItemsResolver
     private TemplateExpressionEvaluator expressionEvaluator;
     private NamespaceService namespaceService;
     private JournalService journalService;
+
+    private LoadingCache<RequestKey, Long> itemsCount;
+
+    public AbstractJournalsResolver() {
+        this.itemsCount = CacheBuilder.newBuilder()
+                .expireAfterWrite(30, TimeUnit.SECONDS)
+                .maximumSize(1000)
+                .build(CacheLoader.from(this::journalItemsCount));
+    }
+
+    public void invalidateAll() {
+        itemsCount.invalidateAll();
+    }
+
+    public void invalidate(String journalId) {
+        itemsCount.invalidate(new RequestKey(journalId));
+    }
 
     protected Element constructItem(NodeRef journalRef, Map<String, String> params, Element context) {
         /* get data */
@@ -44,6 +63,12 @@ public abstract class AbstractJournalsResolver extends AbstractMenuItemsResolver
         String parentElemId = StringUtils.defaultString(context.getId());
         String elemId = String.format("%s_%s_JOURNAL", parentElemId, elemIdVar);
         Boolean displayCount = Boolean.parseBoolean(getParam(params, context, "displayCount"));
+        String countForJournalsParam = getParam(params, context, "countForJournals");
+        Set<String> countForJournals;
+        if (displayCount && StringUtils.isNotEmpty(countForJournalsParam)) {
+            countForJournals = new HashSet<>(Arrays.asList(countForJournalsParam.split(",")));
+            displayCount = countForJournals.contains(journalId);
+        }
         Boolean displayIcon = context.getParams().containsKey("rootElement");
 
         /* icon. if journal element is placed in root category */
@@ -69,8 +94,9 @@ public abstract class AbstractJournalsResolver extends AbstractMenuItemsResolver
 
         /* badge (items count) */
         if (displayCount) {
-            Integer itemsCount = journalItemsCount(journalRef, journalId).intValue();
-            elementParams.put("count", itemsCount.toString());
+            RequestKey requestKey = new RequestKey(journalId);
+            Long count = itemsCount.getUnchecked(requestKey);
+            elementParams.put("count", count.toString());
         }
 
         /* additional params for constructing child items */
@@ -86,7 +112,9 @@ public abstract class AbstractJournalsResolver extends AbstractMenuItemsResolver
         return element;
     }
 
-    private Long journalItemsCount(NodeRef journalRef, String journalId) {
+    private Long journalItemsCount(RequestKey requestKey) {
+        String journalId = requestKey.getJournalId();
+        NodeRef journalRef = journalService.getJournalRef(journalId);
         String query = buildJournalQuery(journalRef);
         RecordsResult<RecordRef> result = journalService.getRecords(journalId, query, null, PAGE_INFO);
         return result.getTotalCount();
@@ -146,5 +174,47 @@ public abstract class AbstractJournalsResolver extends AbstractMenuItemsResolver
     @Autowired
     public void setNamespaceService(NamespaceService namespaceService) {
         this.namespaceService = namespaceService;
+    }
+
+    private class RequestKey {
+
+        private final String userName;
+        private final String journalId;
+
+        public RequestKey(String journalId) {
+            Objects.requireNonNull(journalId);
+            this.userName = StringUtils.defaultString(AuthenticationUtil.getFullyAuthenticatedUser());
+            this.journalId = journalId;
+        }
+
+        public RequestKey(String userName, String journalId) {
+            Objects.requireNonNull(journalId);
+            Objects.requireNonNull(userName);
+            this.userName = userName;
+            this.journalId = journalId;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = 629 + userName.hashCode();
+            result = 37 * result + journalId.hashCode();
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return obj instanceof RequestKey &&
+                    (userName.equals(((RequestKey) obj).userName)) &&
+                    (journalId.equals(((RequestKey) obj).journalId));
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public String getJournalId() {
+            return journalId;
+        }
+
     }
 }
