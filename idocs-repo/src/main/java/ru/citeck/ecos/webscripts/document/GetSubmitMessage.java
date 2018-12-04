f2c0d406686bc41650c9a7833086ba2a262b39d2
Denis Rulev <denis.rulev@citeck.ru>
2018-12-04 13:30:37 +0400

    ECOSCOM-1850 Process start message

--- idocs-repo/src/main/java/ru/citeck/ecos/webscripts/document/GetSubmitMessage.java
+++ idocs-repo/src/main/java/ru/citeck/ecos/webscripts/document/GetSubmitMessage.java
@@ -1,25 +1,23 @@
 package ru.citeck.ecos.webscripts.document;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.alfresco.service.cmr.dictionary.ClassDefinition;
 import org.alfresco.service.cmr.dictionary.DictionaryService;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
 import org.alfresco.service.namespace.NamespaceService;
 import org.alfresco.service.namespace.QName;
-import org.json.JSONObject;
 import org.springframework.extensions.surf.util.I18NUtil;
-import org.springframework.extensions.webscripts.AbstractWebScript;
-import org.springframework.extensions.webscripts.WebScriptException;
-import org.springframework.extensions.webscripts.WebScriptRequest;
-import org.springframework.extensions.webscripts.WebScriptResponse;
+import org.springframework.extensions.webscripts.*;
 import ru.citeck.ecos.spring.registry.MappingRegistry;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.Map;
 
-import java.util.stream.Collectors;
-
 public class GetSubmitMessage extends AbstractWebScript {
 
+    private ObjectMapper objectMapper = new ObjectMapper();
     private NodeService nodeService;
     private NamespaceService namespaceService;
     private DictionaryService dictionaryService;
@@ -32,47 +30,43 @@ public class GetSubmitMessage extends AbstractWebScript {
         String docRefParam = req.getParameter(NODE_REF);
         NodeRef document = new NodeRef(docRefParam);
         try {
-            // construct resulting JSON
             res.setContentType("application/json");
             res.setContentEncoding("UTF-8");
-            res.addHeader("Cache-Control", "no-cache");
+            res.setCache(new Cache());
             res.addHeader("Pragma", "no-cache");
-            // write JSON into response stream
-            JSONObject result = buildResult(getMessageKey(document));
-            result.write(res.getWriter());
+            Result result = buildResult(getMessageKey(document));
+            objectMapper.writeValue(res.getOutputStream(), result);
         } catch (Exception e) {
             throw new WebScriptException("Caught exception", e);
         }
     }
 
-    private JSONObject buildResult(String messageKey) throws Exception{
-        JSONObject result = new JSONObject();
+    private Result buildResult(String messageKey) {
+        Result result = new Result();
         if (messageKey != null) {
             String message = I18NUtil.getMessage(messageKey);
             if (!messageKey.equals("disabled") && message != null && message.length() > 0) {
-                result.put("disabled", false);
-                result.put("message", message);
+                result.setDisabled(false);
+                result.setMessage(message);
             } else {
-                result.put("disabled", true);
-                result.put("message", "");
+                result.setDisabled(true);
+                result.setMessage("");
             }
         } else {
-            result.put("disabled", true);
-            result.put("message", "");
+            result.setDisabled(true);
+            result.setMessage("");
         }
         return result;
     }
 
     private String getMessageKey(NodeRef document) {
         QName documentType = nodeService.getType(document);
+        String docShortQName = documentType.toPrefixString(namespaceService);
 
-        Map<QName, String> mapping = documentToMessage.getMapping().entrySet().stream().collect(Collectors.toMap(
-                entry -> QName.resolveToQName(namespaceService, entry.getKey()),
-                entry -> (entry.getValue())
-        ));
+        Map<String, String> mapping = documentToMessage.getMapping();
 
-        if (mapping.containsKey(documentType)) {
-            return mapping.get(documentType);
+        if (mapping.containsKey(docShortQName)) {
+            return mapping.get(docShortQName);
         }
 
         ClassDefinition classDefinition = dictionaryService.getClass(documentType);
@@ -80,12 +74,18 @@ public class GetSubmitMessage extends AbstractWebScript {
             classDefinition = classDefinition.getParentClassDefinition();
             if (classDefinition != null) {
                 QName currentName = classDefinition.getName();
-                if (mapping.containsKey(currentName)) {
-                    return mapping.get(currentName);
+                String currentShortQName = currentName.toPrefixString(namespaceService);
+                if (mapping.containsKey(currentShortQName)) {
+                    return mapping.get(currentShortQName);
                 }
             }
         }
         return null;
+    }
+
+    private static class Result {
+        @Setter @Getter private boolean disabled;
+        @Setter @Getter private String message;
     }
 
     public NodeService getNodeService() {
