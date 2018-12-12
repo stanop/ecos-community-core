66c43a04a1ed2ba63dc1ebf9456a9b69a38fd412
Ivan Tkachenko <ivan.tkachenko@citeck.ru>
2018-12-12 13:17:07 +0300

    ECOSCOM-1890 - Fix mobile styles jumping

--- idocs-share/src/main/resources/META-INF/js/citeck/modules/header/share-header.jsx
+++ idocs-share/src/main/resources/META-INF/js/citeck/modules/header/share-header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import CreateCaseWidget from './components/create-case-widget';
 import UserMenu from './components/user-menu';
 import SitesMenu from './components/sites-menu';
@@ -7,12 +8,17 @@ import "xstyle!js/citeck/modules/header/share-header.css";
 
 import CustomModal from './components/custom-modal';
 
-const ShareHeader = () => {
+const mapStateToProps = state => ({
+    isMobile: state.view.isMobile
+});
+
+const ShareHeader = ({ isMobile }) => {
+    const hamburgerIcon = isMobile ? <label className='hamburger-icon' htmlFor="slide-menu-checkbox" /> : null;
     return (
         <div id='SHARE_HEADER' className='alfresco-header-Header'>
             <div className="alfresco-layout-LeftAndRight__left">
                 <div id="HEADER_APP_MENU_BAR">
-                    <label className='hamburger-icon' htmlFor="slide-menu-checkbox" />
+                    {hamburgerIcon}
                 </div>
                 <CreateCaseWidget />
             </div>
@@ -26,4 +32,4 @@ const ShareHeader = () => {
     );
 };
 
-export default ShareHeader;
+export default connect(mapStateToProps)(ShareHeader);
