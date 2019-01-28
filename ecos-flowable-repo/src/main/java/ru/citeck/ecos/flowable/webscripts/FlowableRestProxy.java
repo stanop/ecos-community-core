e770250c8e5a09b87f0269557354d85092d58cc9
Pavel Simonov <pavel.simonov@citeck.ru>
2019-01-28 17:00:55 +0700

    ECOSCOM-1943 - BPMN designer improvements

--- ecos-flowable-repo/src/main/java/ru/citeck/ecos/flowable/webscripts/FlowableRestProxy.java
+++ ecos-flowable-repo/src/main/java/ru/citeck/ecos/flowable/webscripts/FlowableRestProxy.java
@@ -1,26 +1,45 @@
 package ru.citeck.ecos.flowable.webscripts;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.alfresco.repo.admin.SysAdminParams;
-import org.alfresco.repo.content.MimetypeMap;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.extensions.webscripts.*;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import ru.citeck.ecos.flowable.services.rest.FlowableRestTemplate;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class FlowableRestProxy extends AbstractWebScript {
 
     private static final String WEBSCRIPT_URL = "citeck/flowable/proxy/";
     private static final String FLOWABLE_HOST_KEY = "${flowable.host.url}";
+
+    private static final String HEADER_CONTENT_DISPOSITION = "Content-Disposition";
+
+    private static final String[] HEADERS_TO_SEND = {
+            HttpHeaders.ACCEPT,
+            HttpHeaders.ACCEPT_CHARSET,
+            HttpHeaders.ACCEPT_ENCODING,
+            HttpHeaders.ACCEPT_LANGUAGE,
+            HttpHeaders.ACCEPT_RANGES,
+            HttpHeaders.AGE,
+            HttpHeaders.ALLOW,
+            HttpHeaders.CACHE_CONTROL,
+            HttpHeaders.CONNECTION,
+            HttpHeaders.CONTENT_ENCODING,
+            HttpHeaders.CONTENT_LANGUAGE,
+            HttpHeaders.CONTENT_LENGTH,
+            HttpHeaders.CONTENT_LOCATION,
+            HttpHeaders.CONTENT_RANGE,
+            HttpHeaders.EXPECT
+    };
 
     @Value(FLOWABLE_HOST_KEY)
     private String flowableHost;
@@ -31,10 +50,8 @@ public class FlowableRestProxy extends AbstractWebScript {
     @Qualifier("sysAdminParams")
     private SysAdminParams sysAdminParams;
 
-    private ObjectMapper mapper = new ObjectMapper();
-
     @Override
-    public void execute(WebScriptRequest req, WebScriptResponse res) throws IOException {
+    public void execute(WebScriptRequest req, WebScriptResponse res) {
 
         if (flowableHost == null || flowableHost.isEmpty() || FLOWABLE_HOST_KEY.equals(flowableHost)) {
             flowableHost = sysAdminParams.getAlfrescoProtocol() + "://" +
@@ -42,60 +59,77 @@ public class FlowableRestProxy extends AbstractWebScript {
                            sysAdminParams.getAlfrescoPort();
         }
 
-        HttpMethod method;
         String desc = getDescription().getId();
-        if (desc.endsWith(".post")) {
-            method = HttpMethod.POST;
-        } else if (desc.endsWith(".put")) {
-            method = HttpMethod.PUT;
-        } else {
-            method = HttpMethod.GET;
-        }
-
-        String contentType = req.getParameter("_proxy_content_type");
-        if (contentType == null || contentType.isEmpty()) {
-            contentType = req.getHeader("Content-Type");
-        }
-
-        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
-        headers.put("Accept", Arrays.asList(req.getHeaderValues("Accept")));
-        headers.add("Content-Type", contentType);
-        HttpEntity<Object> requestEntity = new HttpEntity<>(getBody(req), headers);
+        String ext = FilenameUtils.getExtension(desc);
+        HttpMethod method = HttpMethod.valueOf(ext.toUpperCase());
 
         String url = getFlowableUrl(req.getURL());
-        ResponseEntity<String> entity = restTemplate.exchange(url, method, requestEntity, String.class);
-
-        entity.getHeaders().forEach((key, value) -> {
-            if ("Content-Type".equals(key) && value.size() > 0) {
-                res.setContentType(value.get(0));
-            }
-        });
-
         res.setContentEncoding("UTF-8");
-        res.getWriter().write(entity.getBody());
-        res.setStatus(entity.getStatusCode().value());
-    }
 
-    private Object getBody(WebScriptRequest req) throws IOException {
+        restTemplate.execute(url, method, request -> {
 
-        String contentType = req.getContentType();
-        if (contentType == null) {
-            contentType = "";
-        }
+            for (String headerName : HEADERS_TO_SEND) {
+                String header = req.getHeader(headerName);
+                if (StringUtils.isNotBlank(header)) {
+                    request.getHeaders().add(headerName, header);
+                }
+            }
 
-        String bodyStr = req.getContent().getContent();
-        if (bodyStr == null || bodyStr.length() == 0) {
-            return "";
-        }
+            boolean hasBody = false;
 
-        Object body;
-        if (contentType.contains(MimetypeMap.MIMETYPE_JSON)) {
-            body = mapper.readTree(bodyStr);
-        } else {
-            body = bodyStr;
-        }
+            try (InputStream in = req.getContent().getInputStream()) {
 
-        return body;
+                if (in.available() > 0) {
+                    IOUtils.copy(in, request.getBody());
+                    hasBody = true;
+                }
+            } catch (IOException e) {
+                //do nothing
+            }
+
+            if (hasBody) {
+
+                String contentType = req.getParameter("_proxy_content_type");
+
+                if (StringUtils.isBlank(contentType)) {
+                    contentType = req.getHeader(HttpHeaders.CONTENT_TYPE);
+                }
+
+                if (StringUtils.isNotBlank(contentType)) {
+                    request.getHeaders().add(HttpHeaders.CONTENT_TYPE, contentType);
+                }
+            }
+        }, response -> {
+
+            response.getHeaders().forEach((key, values) -> {
+                if (values.size() == 0) {
+                    return;
+                }
+                String value = values.get(0);
+                switch (key) {
+                    case HttpHeaders.CONTENT_TYPE:
+                        res.setContentType(value);
+                        break;
+                    case HttpHeaders.CONTENT_LENGTH:
+                        res.setHeader(key, value);
+                        break;
+                    case HttpHeaders.CONTENT_ENCODING:
+                        res.setContentEncoding(value);
+                        break;
+                    case HEADER_CONTENT_DISPOSITION:
+                        res.setHeader(HEADER_CONTENT_DISPOSITION, value);
+                        break;
+                }
+            });
+
+            try (OutputStream out = res.getOutputStream()) {
+                IOUtils.copy(response.getBody(), out);
+            }
+
+            res.setStatus(response.getStatusCode().value());
+
+            return null;
+        });
     }
 
     private String getFlowableUrl(String baseUrl) {
