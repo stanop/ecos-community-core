ca6ecbecc7d7316f2637d27c308bc5448af4235d
Pavel Simonov <pavel.simonov@citeck.ru>
2019-03-13 08:49:48 +0700

    ECOSCOM-2044 - Add API to request grouped data from recordsService

--- ecos-base-core-repo/src/main/java/ru/citeck/ecos/records/RecordsConfiguration.java
+++ ecos-base-core-repo/src/main/java/ru/citeck/ecos/records/RecordsConfiguration.java
@@ -4,6 +4,7 @@ import org.alfresco.service.ServiceRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.citeck.ecos.graphql.AlfGqlContext;
+import ru.citeck.ecos.predicate.PredicateService;
 import ru.citeck.ecos.records2.RecordsServiceFactory;
 import ru.citeck.ecos.records2.graphql.RecordsMetaGql;
 import ru.citeck.ecos.records2.meta.RecordsMetaService;
@@ -15,9 +16,15 @@ public class RecordsConfiguration extends RecordsServiceFactory {
 
     @Bean
     public ru.citeck.ecos.records.RecordsServiceImpl createRecordsServiceBean(ServiceRegistry serviceRegistry,
-                                                                              RecordsMetaService recordsMetaService) {
+                                                                              RecordsMetaService recordsMetaService,
+                                                                              PredicateService predicateService) {
         this.serviceRegistry = serviceRegistry;
-        return new RecordsServiceImpl(recordsMetaService);
+        return new RecordsServiceImpl(recordsMetaService, predicateService);
+    }
+
+    @Bean
+    public PredicateService createPredicateService() {
+        return super.createPredicateService();
     }
 
     @Bean
