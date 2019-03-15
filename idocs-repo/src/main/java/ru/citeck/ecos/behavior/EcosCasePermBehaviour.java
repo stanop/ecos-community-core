aef3fcad2697515a98bebfaf523752cb2eb0264e
Pavel Simonov <pavel.simonov@citeck.ru>
2019-03-15 08:03:19 +0700

    ECOSCOM-2036 - New created case types errors

---
+++ idocs-repo/src/main/java/ru/citeck/ecos/behavior/EcosCasePermBehaviour.java
@@ -0,0 +1,101 @@
+package ru.citeck.ecos.behavior;
+
+import org.alfresco.model.ContentModel;
+import org.alfresco.repo.node.NodeServicePolicies;
+import org.alfresco.repo.policy.Behaviour;
+import org.alfresco.service.cmr.repository.ChildAssociationRef;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.security.PermissionService;
+import org.alfresco.service.namespace.QName;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.citeck.ecos.behavior.base.AbstractBehaviour;
+import ru.citeck.ecos.behavior.base.PolicyMethod;
+import ru.citeck.ecos.config.EcosConfigService;
+import ru.citeck.ecos.model.EcosModel;
+import ru.citeck.ecos.model.IdocsModel;
+import ru.citeck.ecos.records2.utils.StringUtils;
+import ru.citeck.ecos.utils.NodeUtils;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class EcosCasePermBehaviour extends AbstractBehaviour implements NodeServicePolicies.OnCreateNodePolicy {
+
+    private static final String ENABLED_CONFIG_KEY = "ecosCaseFolderPermissionConfig";
+    private static final String NODE_MANAGER_PERM = "NodeManager";
+
+    private EcosConfigService ecosConfigService;
+    private PermissionService permissionService;
+    private NodeUtils nodeUtils;
+
+    @Override
+    protected void beforeInit() {
+        setClassName(EcosModel.TYPE_CASE);
+        permissionService = serviceRegistry.getPermissionService();
+    }
+
+    @PolicyMethod(policy = NodeServicePolicies.OnCreateNodePolicy.class,
+                  frequency = Behaviour.NotificationFrequency.TRANSACTION_COMMIT)
+    public void onCreateNode(ChildAssociationRef childAssociationRef) {
+
+        Object paramValue = ecosConfigService.getParamValue(ENABLED_CONFIG_KEY);
+        if (!"true".equals(paramValue)) {
+            return;
+        }
+
+        NodeRef caseRef = childAssociationRef.getChildRef();
+        Map<QName, Serializable> caseProps = nodeService.getProperties(caseRef);
+        String creator = (String) caseProps.get(ContentModel.PROP_CREATOR);
+
+        if (StringUtils.isBlank(creator)) {
+            return;
+        }
+
+        NodeRef parentRef = nodeService.getPrimaryParent(caseRef).getParentRef();
+        Map<QName, Serializable> parentProps = nodeService.getProperties(parentRef);
+
+        String parentName = (String) parentProps.get(ContentModel.PROP_NAME);
+
+        if (creator.equals(parentName)) {
+            return;
+        }
+
+        NodeRef creatorFolder = nodeService.getChildByName(parentRef, ContentModel.ASSOC_CONTAINS, creator);
+
+        if (creatorFolder == null) {
+
+            Map<QName, Serializable> properties = new HashMap<>();
+            properties.put(ContentModel.PROP_NAME, creator);
+
+            creatorFolder = nodeUtils.createNode(parentRef,
+                                                 ContentModel.TYPE_FOLDER,
+                                                 ContentModel.ASSOC_CONTAINS,
+                                                 properties);
+
+            permissionService.setInheritParentPermissions(creatorFolder, false);
+            permissionService.setPermission(creatorFolder, creator, NODE_MANAGER_PERM, true);
+
+            String managerGroup = (String) parentProps.get(IdocsModel.PROP_MANAGER_GROUP);
+            String managerPermission = (String) parentProps.get(IdocsModel.PROP_MANAGER_PERMISSION);
+
+            if (StringUtils.isNotBlank(managerGroup) && StringUtils.isNotBlank(managerPermission)) {
+                permissionService.setPermission(creatorFolder, managerGroup, managerPermission, true);
+            }
+        }
+
+        String caseName = (String) caseProps.get(ContentModel.PROP_NAME);
+        QName assocQName = QName.createQName(EcosModel.ECOS_NAMESPACE, caseName);
+        nodeService.moveNode(caseRef, creatorFolder, ContentModel.ASSOC_CONTAINS, assocQName);
+    }
+
+    @Autowired
+    public void setNodeUtils(NodeUtils nodeUtils) {
+        this.nodeUtils = nodeUtils;
+    }
+
+    @Autowired
+    public void setEcosConfigService(EcosConfigService ecosConfigService) {
+        this.ecosConfigService = ecosConfigService;
+    }
+}
