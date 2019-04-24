55170df96d6a696c9651f65ab93ba39819df319a
Pavel Simonov <pavel.simonov@citeck.ru>
2019-04-24 12:51:43 +0700

    ECOSCOM-2155 - [Forms] Add formMode option to forms

---
+++ ecos-base-core-repo/src/main/java/ru/citeck/ecos/records/RecordConstants.java
@@ -0,0 +1,10 @@
+package ru.citeck.ecos.records;
+
+public interface RecordConstants {
+
+    String ATT_PARENT = "_parent";
+    String ATT_PARENT_ATT = "_parentAtt";
+    String ATT_TYPE = "_type";
+    String ATT_FORM_KEY = "_formKey";
+    String ATT_VIEW_FORM_KEY = "_viewFormKey";
+}
