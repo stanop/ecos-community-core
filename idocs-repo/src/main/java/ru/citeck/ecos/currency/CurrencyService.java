b01732a54dec8c23f3252dcc013f9b76150a8e04
Roman Makarskiy <roman.makarskiy@citeck.ru>
2019-05-07 19:09:32 +0300

    ECOSCOM-2178 - Add ability to get LastCurrencyRateWithManualConversion

--- idocs-repo/src/main/java/ru/citeck/ecos/currency/CurrencyService.java
+++ idocs-repo/src/main/java/ru/citeck/ecos/currency/CurrencyService.java
@@ -12,7 +12,7 @@ import java.util.Date;
  * system, and other things.
  *
  * @author alexander.nemerov
- *         date 03.11.2016.
+ * date 03.11.2016.
  */
 public interface CurrencyService {
 
@@ -35,4 +35,19 @@ public interface CurrencyService {
     Currency getCurrencyByNodeRef(NodeRef nodeRef);
 
     BigDecimal getLastCurrencyRate(NodeRef baseCurrency, NodeRef targetCurrency, Date date);
+
+    /**
+     * Method to get last currency rate with manual conversion.</br>
+     * Manual conversion implies that if no pair of rate record baseCurrency-targetCurrency found,</br>
+     * then trying to find reversed pair targetCurrency-baseCurrency and calculate currency rate </br>
+     * (with scale = 4, round = {@link BigDecimal#ROUND_HALF_UP}).
+     * <p>
+     * If no rate found in currency records, then return rate by {@link Currency}
+     *
+     * @param baseCurrency   base currency
+     * @param targetCurrency target currency
+     * @param date           currency rate date
+     * @return last currency rate
+     */
+    BigDecimal getLastCurrencyRateWithManualConversion(NodeRef baseCurrency, NodeRef targetCurrency, Date date);
 }
