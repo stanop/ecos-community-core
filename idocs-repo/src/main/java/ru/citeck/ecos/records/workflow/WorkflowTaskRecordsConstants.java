a5dca3a39c4c50629aaeb828bf3dd9ac242e5bf1
Roman Makarskiy <roman.makarskiy@citeck.ru>
2019-06-26 14:09:50 +0300

    ECOSCOM-2287 - task widget api

---
+++ idocs-repo/src/main/java/ru/citeck/ecos/records/workflow/WorkflowTaskRecordsConstants.java
@@ -0,0 +1,29 @@
+package ru.citeck.ecos.records.workflow;
+
+class WorkflowTaskRecordsConstants {
+
+    private WorkflowTaskRecordsConstants() {
+    }
+
+    static final String SPACES_STORE_PREFIX = "workspace://SpacesStore/";
+
+    static final String ATT_DOC_SUM = "docSum";
+    static final String ATT_DOC_STATUS = "docStatus";
+    static final String ATT_DOC_DISP_NAME = "docDisplayName";
+    static final String ATT_DOC_STATUS_TITLE = "docStatusTitle";
+    static final String ATT_DOC_TYPE = "docType";
+
+    static final String ATT_SENDER = "sender";
+    static final String ATT_STARTED = "started";
+    static final String ATT_DUE_DATE = "dueDate";
+    static final String ATT_ASSIGNEE = "assignee";
+    static final String ATT_LASTCOMMENT = "lastcomment";
+    static final String ATT_TITLE = "title";
+    static final String ATT_REASSIGNABLE = "reassignable";
+    static final String ATT_RELEASABLE = "releasable";
+    static final String ATT_CLAIMABLE = "claimable";
+    static final String ATT_ACTIVE = "active";
+
+    static final String CURRENT_USER = "$CURRENT";
+
+}
