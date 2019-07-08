5e3852fac10e47cda045b1be63b8da2148256e61
Roman Makarskiy <roman.makarskiy@citeck.ru>
2019-07-08 11:21:39 +0300

    ECOSCOM-2311 - Add API to get available statuses for node type

---
+++ idocs-repo/src/main/java/ru/citeck/ecos/records/status/StatusDTO.java
@@ -0,0 +1,17 @@
+package ru.citeck.ecos.records.status;
+
+import lombok.Data;
+
+/**
+ * @author Roman Makarskiy
+ */
+@Data
+public class StatusDTO {
+
+    private String id;
+
+    private String name;
+
+    private String type;
+
+}
