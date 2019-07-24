7fe97debd15425d0f81f1078dc2e1fa1b4bbfde8
Pavel Simonov <pavel.simonov@citeck.ru>
2019-07-24 15:50:08 +0700

    ECOSCOM-2385 - Migrate ecos-ui from extdev to common image

--- docker/ecos-community/docker-desktop-up.bat
+++ docker/ecos-community/docker-desktop-up.bat
@@ -1,5 +1,5 @@
 SET LOCAL_HOST=host.docker.internal
 SET ALFRESCO_TARGET=%LOCAL_HOST%:8080
-SET GATEWAY_TARGET=%LOCAL_HOST%
+SET GATEWAY_TARGET=%LOCAL_HOST%:8085
 docker-compose pull
 docker-compose up
