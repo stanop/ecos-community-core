fe5625c456c6b476b6c15900b552167df626570b
Aleksey Zaytsev <alekseiz_000@mail.ru>
2019-08-27 09:53:53 +0700

    ECOSCOM-2562 - Improving the quality of source code Magic Constant (grafted from ec6e0f758a15de85242d9543345e9d124955e5dd)

--- idocs-repo/src/main/java/ru/citeck/ecos/calendar/BusinessCalendar.java
+++ idocs-repo/src/main/java/ru/citeck/ecos/calendar/BusinessCalendar.java
@@ -69,7 +69,7 @@ public class BusinessCalendar extends GregorianCalendar {
 
 	public boolean isBusinessDay()
 	{
-		if(this.get(DAY_OF_WEEK)==1 || this.get(DAY_OF_WEEK)==7)
+		if(this.get(DAY_OF_WEEK) == Calendar.SUNDAY || this.get(DAY_OF_WEEK) == Calendar.SATURDAY)
 		{
 			if(!mapContainsDate(extraWorkingDays, this))
 			{
