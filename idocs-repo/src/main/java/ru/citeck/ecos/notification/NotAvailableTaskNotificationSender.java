139a359a2a2adf092bd8611c242602a7f77a70ce
Aleksey Zaytsev <alekseiz_000@mail.ru>
2019-08-29 11:39:13 +0700

    ECOSCOM-2562 - Improving the quality of source code Refactored condition (grafted from 6c39aa214acf0c2c4099ed501b5dd979bf061109)

--- idocs-repo/src/main/java/ru/citeck/ecos/notification/NotAvailableTaskNotificationSender.java
+++ idocs-repo/src/main/java/ru/citeck/ecos/notification/NotAvailableTaskNotificationSender.java
@@ -82,9 +82,11 @@ class NotAvailableTaskNotificationSender extends DelegateTaskNotificationSender 
         String originalOwner = (String) task.getVariable("taskOriginalOwner");
 
         String lastTaskOwner = (String) task.getVariable(lastTaskOwnerVar);
-        if (((lastTaskOwner != null && lastTaskOwner.equals(owner) && originalOwner != null && originalOwner.equals(lastTaskOwner)) || (lastTaskOwner == null && owner != null && originalOwner != null && owner.equals(originalOwner))) && isSendToInitiator(template)) {
+        boolean checkOwner = originalOwner != null && originalOwner.equals(owner) && (lastTaskOwner == null || originalOwner.equals(lastTaskOwner));
+        if (checkOwner && isSendToInitiator(template)) {
             return;
         }
+
         if (isSendToInitiator(template)) {
             Set<String> assignees = getAssignee(task);
             for (String assignee : assignees) {
