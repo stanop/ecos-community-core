740b30ecc1a61464c53b3e742f36e57e7248dfb0
Oleg Raskin <oleg.raskin@citeck.ru>
2019-12-06 14:00:07 +0400

    ECOSCOM-2922 - Barcode API returns 500

--- ecos-base-core-repo/src/main/java/ru/citeck/ecos/processor/exception/BarcodeInputException.java
+++ ecos-base-core-repo/src/main/java/ru/citeck/ecos/processor/exception/BarcodeInputException.java
@@ -2,6 +2,10 @@ package ru.citeck.ecos.processor.exception;
 
 public class BarcodeInputException extends RuntimeException {
 
+    public BarcodeInputException(String message) {
+        super(message);
+    }
+
     public BarcodeInputException(Exception e) {
         super("Cannot extract input value for generate barcode", e);
     }
