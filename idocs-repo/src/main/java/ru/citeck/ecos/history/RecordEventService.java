72e7d4e9eb819dec416bde588b3215e8bde7eb08
Roman Makarskiy <Romanchabest55@gmail.com>
2019-12-26 14:56:13 +0300

    ECOSCOM-2913 - Implement send update to facade

--- idocs-repo/src/main/java/ru/citeck/ecos/history/RecordEventService.java
+++ idocs-repo/src/main/java/ru/citeck/ecos/history/RecordEventService.java
@@ -63,13 +63,13 @@ public class RecordEventService {
     }
 
     public void emitAttrChanged(RecordEventType eventType, String docId, Set<String> attrChanged) {
-        if (eventConnection == null) {
-            throw new RuntimeException("Sending event of updating attributes is required," +
-                    " but connection to event server is not enabled. Check you configs.");
-        }
-
         if (CollectionUtils.isEmpty(attrChanged) || !emitEnabled(RecordEventType.UPDATE)) {
             return;
+        }
+
+        if (eventConnection == null) {
+            throw new RuntimeException("Sending event of updating attributes is required," +
+                " but connection to event server is not enabled. Check you configs.");
         }
 
         log.debug("Record event emit, docId:{}, atts:{}", docId, attrChanged);
