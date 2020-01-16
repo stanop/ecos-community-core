b70524821e316a9d274c3dfb95d1335df3e8a867
Roman Makarskiy <Romanchabest55@gmail.com>
2020-01-16 10:07:17 +0300

    ECOSCOM-3029 - Add logging recipients to TaskNotificationSender

--- idocs-repo/src/main/java/ru/citeck/ecos/workflow/utils/OverdueNotificationJob.java
+++ idocs-repo/src/main/java/ru/citeck/ecos/workflow/utils/OverdueNotificationJob.java
@@ -18,67 +18,61 @@
  */
 package ru.citeck.ecos.workflow.utils;
 
+import lombok.extern.slf4j.Slf4j;
 import org.alfresco.repo.security.authentication.AuthenticationUtil;
-import org.alfresco.repo.security.authentication.AuthenticationUtil.RunAsWork;
 import org.alfresco.repo.workflow.WorkflowModel;
 import org.alfresco.service.cmr.workflow.WorkflowService;
 import org.alfresco.service.cmr.workflow.WorkflowTask;
 import org.alfresco.service.cmr.workflow.WorkflowTaskQuery;
 import org.alfresco.service.cmr.workflow.WorkflowTaskQuery.OrderBy;
 import org.alfresco.service.cmr.workflow.WorkflowTaskState;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 import ru.citeck.ecos.job.AbstractLockedJob;
 import ru.citeck.ecos.notification.NotificationSender;
 
 import java.util.Date;
 import java.util.List;
 
-public class OverdueNotificationJob extends AbstractLockedJob
-{
-    private static Log logger = LogFactory.getLog(OverdueNotificationJob.class);
+@Slf4j
+public class OverdueNotificationJob extends AbstractLockedJob {
 
     private static final Object PARAM_NOTIFICATION_SENDER = "NotificationSender";
     private static final Object PARAM_WORKFLOW_SERVICE = "WorkflowService";
 
     @Override
-    public void executeJob(JobExecutionContext context) throws JobExecutionException {
+    public void executeJob(JobExecutionContext context) {
+        log.debug("OverdueNotificationJob is started");
+
         JobDataMap data = context.getJobDetail().getJobDataMap();
 
         final WorkflowService workflowService = (WorkflowService) data.get(PARAM_WORKFLOW_SERVICE);
         final NotificationSender<WorkflowTask> sender = (NotificationSender<WorkflowTask>) data.get(PARAM_NOTIFICATION_SENDER);
 
-        Integer sent = AuthenticationUtil.runAs(new RunAsWork<Integer>() {
-
-            @Override
-            public Integer doWork() throws Exception {
-                WorkflowTaskQuery query = new WorkflowTaskQuery();
-                query.setTaskState(WorkflowTaskState.IN_PROGRESS);
-                query.setOrderBy(new OrderBy[] { OrderBy.TaskDue_Asc });
-                List<WorkflowTask> tasks = workflowService.queryTasks(query);
-                Date now = new Date();
-                int sent = 0;
-                for (WorkflowTask task : tasks) {
-                    Date dueDate = (Date) task.getProperties().get(WorkflowModel.PROP_DUE_DATE);
-                    logger.debug("dueDate " + dueDate);
-                    if (dueDate != null) {
-                        dueDate.setHours(23); // use 23:59, not 00:00
-                        dueDate.setMinutes(59);
-                        if (dueDate.before(now)) {
-                            logger.debug("dueDate " + dueDate + " before now " + now);
-                            sender.sendNotification(task);
-                            sent++;
-                        }
+        Integer sentTasks = AuthenticationUtil.runAs(() -> {
+            WorkflowTaskQuery query = new WorkflowTaskQuery();
+            query.setTaskState(WorkflowTaskState.IN_PROGRESS);
+            query.setOrderBy(new OrderBy[]{OrderBy.TaskDue_Asc});
+            List<WorkflowTask> tasks = workflowService.queryTasks(query);
+            Date now = new Date();
+            int sent = 0;
+            for (WorkflowTask task : tasks) {
+                Date dueDate = (Date) task.getProperties().get(WorkflowModel.PROP_DUE_DATE);
+                log.debug("dueDate " + dueDate);
+                if (dueDate != null && !"".equals(dueDate)) {
+                    dueDate.setHours(23); // use 23:59, not 00:00
+                    dueDate.setMinutes(59);
+                    if (dueDate.before(now)) {
+                        log.debug("dueDate " + dueDate + " before now " + now);
+                        sender.sendNotification(task);
+                        sent++;
                     }
                 }
-                return sent;
             }
-
+            return sent;
         }, AuthenticationUtil.getSystemUserName());
-        logger.debug("Sent notifications for " + sent + " overdue tasks");
+        log.debug("Sent notifications for " + sentTasks + " overdue tasks");
+        log.debug("OverdueNotificationJob is finished");
     }
 
 }
