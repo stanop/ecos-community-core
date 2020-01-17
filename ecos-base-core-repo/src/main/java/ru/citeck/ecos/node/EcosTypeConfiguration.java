d9baa48a2098194d1957a2789e0574e4a50a7013
Oleg Raskin <raskin.o@yandex.ru>
2020-01-17 11:40:36 +0400

    ECOSCOM-3011 - Associations widget doesn't work for supplementary agreement

---
+++ ecos-base-core-repo/src/main/java/ru/citeck/ecos/node/EcosTypeConfiguration.java
@@ -0,0 +1,45 @@
+package ru.citeck.ecos.node;
+
+import org.alfresco.model.ContentModel;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.namespace.QName;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import ru.citeck.ecos.model.ClassificationModel;
+import ru.citeck.ecos.records2.RecordRef;
+
+import javax.annotation.PostConstruct;
+import java.io.Serializable;
+import java.util.Map;
+
+@Configuration
+public class EcosTypeConfiguration {
+
+    @Autowired
+    private EcosTypeService ecosTypeService;
+
+    @PostConstruct
+    public void init() {
+        ecosTypeService.register(ContentModel.TYPE_CMOBJECT, this::evalDefaultEcosType);
+    }
+
+    private RecordRef evalDefaultEcosType(AlfNodeInfo info) {
+
+        Map<QName, Serializable> props = info.getProperties();
+
+        NodeRef type = (NodeRef) props.get(ClassificationModel.PROP_DOCUMENT_TYPE);
+
+        if (type == null) {
+            return null;
+        }
+
+        String ecosType = type.getId();
+
+        NodeRef kind = (NodeRef) props.get(ClassificationModel.PROP_DOCUMENT_KIND);
+        if (kind != null) {
+            ecosType = ecosType + "/" + kind.getId();
+        }
+
+        return RecordRef.create("emodel", "type", ecosType);
+    }
+}
