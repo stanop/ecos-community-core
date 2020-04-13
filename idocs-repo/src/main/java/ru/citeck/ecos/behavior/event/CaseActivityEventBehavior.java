e2d4db1c6045802a38714f9c67ca08727dbe9448
Lev Bavshin <lev.bavshin@citeck.ru>
2020-04-13 11:38:29 +0700

    ECOSCOM-3429 - [ALF] Connect ECOS Process to CaseActivityService
    * move behaviour between classes (refactoring

--- idocs-repo/src/main/java/ru/citeck/ecos/behavior/event/CaseActivityEventBehavior.java
+++ idocs-repo/src/main/java/ru/citeck/ecos/behavior/event/CaseActivityEventBehavior.java
@@ -4,7 +4,6 @@ import org.alfresco.model.ContentModel;
 import org.alfresco.repo.policy.Behaviour;
 import org.alfresco.repo.policy.PolicyComponent;
 import org.alfresco.service.ServiceRegistry;
-import org.alfresco.service.cmr.dictionary.DictionaryService;
 import org.alfresco.service.cmr.repository.ChildAssociationRef;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
@@ -12,35 +11,25 @@ import org.alfresco.service.namespace.QName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
-import ru.citeck.ecos.action.ActionConditionUtils;
 import ru.citeck.ecos.behavior.ChainingJavaBehaviour;
-import ru.citeck.ecos.behavior.event.trigger.UserActionEventTrigger;
-import ru.citeck.ecos.icase.activity.service.alfresco.EventPolicies;
-import ru.citeck.ecos.icase.activity.service.alfresco.CaseActivityPolicies;
 import ru.citeck.ecos.icase.activity.dto.ActivityRef;
 import ru.citeck.ecos.icase.activity.service.CaseActivityService;
-import ru.citeck.ecos.model.ActivityModel;
-import ru.citeck.ecos.model.EventModel;
+import ru.citeck.ecos.icase.activity.service.alfresco.EventPolicies;
 import ru.citeck.ecos.model.ICaseEventModel;
 import ru.citeck.ecos.service.CiteckServices;
 import ru.citeck.ecos.utils.AlfActivityUtils;
-import ru.citeck.ecos.utils.RepoUtils;
 
 import javax.annotation.PostConstruct;
-import java.util.List;
-import java.util.Map;
 
 /**
  * @author Pavel Simonov
  */
 @Component
 @DependsOn("idocs.dictionaryBootstrap")
-public class CaseActivityEventBehavior implements EventPolicies.OnEventPolicy,
-                                                  CaseActivityPolicies.OnCaseActivityResetPolicy {
+public class CaseActivityEventBehavior implements EventPolicies.OnEventPolicy {
 
     private CaseActivityService caseActivityService;
     private AlfActivityUtils alfActivityUtils;
-    private DictionaryService dictionaryService;
     private PolicyComponent policyComponent;
     private NodeService nodeService;
 
@@ -48,7 +37,6 @@ public class CaseActivityEventBehavior implements EventPolicies.OnEventPolicy,
     public CaseActivityEventBehavior(ServiceRegistry serviceRegistry) {
         this.caseActivityService = (CaseActivityService) serviceRegistry.getService(CiteckServices.CASE_ACTIVITY_SERVICE);
         this.alfActivityUtils = (AlfActivityUtils) serviceRegistry.getService(CiteckServices.ALF_ACTIVITY_UTILS);
-        this.dictionaryService = serviceRegistry.getDictionaryService();
         this.policyComponent = serviceRegistry.getPolicyComponent();
         this.nodeService = serviceRegistry.getNodeService();
     }
@@ -58,15 +46,13 @@ public class CaseActivityEventBehavior implements EventPolicies.OnEventPolicy,
         policyComponent.bindClassBehaviour(EventPolicies.OnEventPolicy.QNAME,
                 ContentModel.TYPE_CMOBJECT,
                 new ChainingJavaBehaviour(this, "onEvent", Behaviour.NotificationFrequency.EVERY_EVENT));
-
-        policyComponent.bindClassBehaviour(CaseActivityPolicies.OnCaseActivityResetPolicy.QNAME,
-                ActivityModel.TYPE_ACTIVITY,
-                new ChainingJavaBehaviour(this, "onCaseActivityReset", Behaviour.NotificationFrequency.EVERY_EVENT));
     }
 
     @Override
     public void onEvent(NodeRef eventRef) {
-        if(!nodeService.exists(eventRef)) return;
+        if (!nodeService.exists(eventRef)) {
+            return;
+        }
 
         ChildAssociationRef parentAssocRef = nodeService.getPrimaryParent(eventRef);
         QName assocType = parentAssocRef.getTypeQName();
@@ -84,53 +70,5 @@ public class CaseActivityEventBehavior implements EventPolicies.OnEventPolicy,
             caseActivityService.reset(activityRef);
             caseActivityService.startActivity(activityRef);
         }
-    }
-
-    @Override
-    public void onCaseActivityReset(NodeRef activityRef) {
-        List<ChildAssociationRef> childAssocs = nodeService.getChildAssocs(activityRef);
-        if (childAssocs == null) {
-            return;
-        }
-        for (ChildAssociationRef assoc : childAssocs) {
-            QName type = nodeService.getType(assoc.getChildRef());
-            if (dictionaryService.isSubClass(type, EventModel.TYPE_USER_ACTION)) {
-                List<NodeRef> data = RepoUtils.getChildrenByAssoc(assoc.getChildRef(),
-                                                                  EventModel.ASSOC_ADDITIONAL_DATA_ITEMS,
-                                                                  nodeService);
-                Map<String, Object> vars = ActionConditionUtils.getTransactionVariables();
-                Object additionalData = vars.get(UserActionEventTrigger.ADDITIONAL_DATA_VARIABLE);
-                for (NodeRef dataItem : data) {
-                    if (!dataItem.equals(additionalData)) {
-                        RepoUtils.deleteNode(dataItem, nodeService);
-                    }
-                }
-            }
-        }
-    }
-
-    @Autowired
-    public void setCaseActivityService(CaseActivityService caseActivityService) {
-        this.caseActivityService = caseActivityService;
-    }
-
-    @Autowired
-    public void setAlfActivityUtils(AlfActivityUtils alfActivityUtils) {
-        this.alfActivityUtils = alfActivityUtils;
-    }
-
-    @Autowired
-    public void setDictionaryService(DictionaryService dictionaryService) {
-        this.dictionaryService = dictionaryService;
-    }
-
-    @Autowired
-    public void setPolicyComponent(PolicyComponent policyComponent) {
-        this.policyComponent = policyComponent;
-    }
-
-    @Autowired
-    public void setNodeService(NodeService nodeService) {
-        this.nodeService = nodeService;
     }
 }
