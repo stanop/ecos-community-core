978342977fb52a43c5d79071d33cda4719abb18c
Pavel Simonov <pavel.simonov@citeck.ru>
2020-04-28 13:31:07 +0700

    ECOSCOM-3381 - Write migration of existing journals to uiserv

--- idocs-repo/src/main/java/ru/citeck/ecos/journals/eapps/EcosJournalsMigration.java
+++ idocs-repo/src/main/java/ru/citeck/ecos/journals/eapps/EcosJournalsMigration.java
@@ -30,7 +30,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Slf4j
-@Component
+//@Component
 public class EcosJournalsMigration implements ModuleMigration {
 
     private static final int BATCH_SIZE = 100;
