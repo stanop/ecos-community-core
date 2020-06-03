dc8cb0f2f9f1b8e2ca904e3e1a74336ea5c3a574
Pavel Simonov <pavel.simonov@citeck.ru>
2020-06-03 12:01:12 +0700

    ECOSCOM-3676 - Widget documents. When choosing a document type, its subspecies (tk: kind) should also appear in the list of downloaded files.

--- ecos-base-core-repo/src/main/java/ru/citeck/ecos/node/EcosTypeService.java
+++ ecos-base-core-repo/src/main/java/ru/citeck/ecos/node/EcosTypeService.java
@@ -1,5 +1,6 @@
 package ru.citeck.ecos.node;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.alfresco.service.ServiceRegistry;
 import org.alfresco.service.cmr.repository.NodeRef;
@@ -7,7 +8,9 @@ import org.alfresco.service.namespace.QName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.citeck.ecos.records2.RecordRef;
+import ru.citeck.ecos.records2.RecordsService;
 
+import java.util.List;
 import java.util.function.Function;
 
 @Service("ecosTypeService")
@@ -17,10 +20,12 @@ public class EcosTypeService {
     public static final QName QNAME = QName.createQName("", "ecosTypeService");
 
     private EvaluatorsByAlfNode<RecordRef> evaluators;
+    private RecordsService recordsService;
 
     @Autowired
-    public EcosTypeService(ServiceRegistry serviceRegistry) {
+    public EcosTypeService(ServiceRegistry serviceRegistry, RecordsService recordsService) {
         evaluators = new EvaluatorsByAlfNode<>(serviceRegistry, node -> null);
+        this.recordsService = recordsService;
     }
 
     public RecordRef getEcosType(NodeRef nodeRef) {
@@ -31,8 +36,31 @@ public class EcosTypeService {
         return evaluators.eval(nodeInfo);
     }
 
+    public void forEachAscRef(RecordRef typeRef, Function<RecordRef, Boolean> action) {
+
+        if (action.apply(typeRef)) {
+            return;
+        }
+
+        TypeParents typeInfo = recordsService.getMeta(typeRef, TypeParents.class);
+        if (typeInfo == null || typeInfo.parents == null) {
+            log.error("ECOS type parents can't be resolved");
+            return;
+        }
+
+        for (RecordRef parentRef : typeInfo.parents) {
+            if (action.apply(parentRef)) {
+                return;
+            }
+        }
+    }
+
     public void register(QName nodeType, Function<AlfNodeInfo, RecordRef> evaluator) {
         evaluators.register(nodeType, evaluator);
     }
 
+    @Data
+    public static class TypeParents {
+        private List<RecordRef> parents;
+    }
 }
