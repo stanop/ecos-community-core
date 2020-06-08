002541ec621ffde3be00efa9044f9ff8e59c67dd
Pavel Marchuk <pavel.marchuk@ge.com>
2020-06-08 10:52:37 +0700

    ECOSCOM-3695 select different templates by metadata (using a predicate)

---
+++ idocs-repo/src/main/java/ru/citeck/ecos/template/EcosSelectTemplateService.java
@@ -0,0 +1,99 @@
+package ru.citeck.ecos.template;
+
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.cmr.repository.StoreRef;
+import org.alfresco.service.cmr.search.SearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.citeck.ecos.commons.json.Json;
+import ru.citeck.ecos.model.ClassificationModel;
+import ru.citeck.ecos.model.DmsModel;
+import ru.citeck.ecos.node.EcosTypeService;
+import ru.citeck.ecos.records2.RecordRef;
+import ru.citeck.ecos.records2.RecordsService;
+import ru.citeck.ecos.records2.predicate.PredicateService;
+import ru.citeck.ecos.records2.predicate.RecordElement;
+import ru.citeck.ecos.records2.predicate.RecordElements;
+import ru.citeck.ecos.records2.predicate.model.Predicate;
+import ru.citeck.ecos.search.ftsquery.FTSQuery;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service("ecosSelectTemplateService")
+public class EcosSelectTemplateService {
+
+    @Autowired
+    private NodeService nodeService;
+
+    @Autowired
+    private EcosTypeService ecosTypeService;
+
+    @Autowired
+    private RecordsService recordsService;
+
+    @Autowired
+    private PredicateService predicateService;
+
+    @Autowired
+    private SearchService searchService;
+
+    public List<NodeRef> getNodeOrderedTemplates(NodeRef nodeRef) {
+        RecordRef ecosType = ecosTypeService.getEcosType(nodeRef);
+        if (ecosType == null) {
+            return Collections.emptyList();
+        }
+
+        List<NodeRef> templateBasedOnType = getTemplateBasedOnType(ecosType);
+        if (templateBasedOnType.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        RecordElements element = new RecordElements(recordsService,
+            Collections.singletonList(RecordRef.valueOf(nodeRef.toString())));
+        return templateBasedOnType.stream()
+            .filter(tNodeRef -> filter(element, tNodeRef))
+            .sorted(Comparator.comparingInt(item -> {
+                Integer priority = (Integer) nodeService.getProperty(item, DmsModel.PROP_PRIORITY);
+                return (priority == null) ? 0 : -priority;
+            }))
+            .collect(Collectors.toList());
+    }
+
+    private boolean filter(RecordElements elements, NodeRef tNodeRef) {
+        if (nodeService.hasAspect(tNodeRef, DmsModel.ASPECT_HAS_PREDICATE)) {
+            String predicateStr = (String) nodeService.getProperty(tNodeRef, DmsModel.PROP_PREDICATE);
+            if (predicateStr != null && !predicateStr.isEmpty()) {
+                Predicate predicate = Json.getMapper().read(predicateStr, Predicate.class);
+                if (predicate != null) {
+                    List<RecordElement> filter = predicateService.filter(elements, predicate);
+                    return !filter.isEmpty();
+                }
+            }
+        } else {
+            return true;
+        }
+        return false;
+    }
+
+    private List<NodeRef> getTemplateBasedOnType(RecordRef ecosType) {
+        String categoryId = ecosType.getId();
+        int index = categoryId.indexOf("/");
+        if (index != -1) {
+            categoryId = categoryId.substring(0, index - 1);
+        }
+        NodeRef category = new NodeRef(StoreRef.STORE_REF_WORKSPACE_SPACESSTORE, categoryId);
+        return FTSQuery.create()
+            .type(DmsModel.TYPE_TEMPLATE).and()
+            .value(ClassificationModel.PROP_DOCUMENT_APPLIES_TO_TYPE, category)
+            .and().open()
+            .isNull(ClassificationModel.PROP_DOCUMENT_APPLIES_TO_KIND).or()
+            .isUnset(ClassificationModel.PROP_DOCUMENT_APPLIES_TO_KIND)
+            .close()
+            .eventual()
+            .query(searchService);
+    }
+}
